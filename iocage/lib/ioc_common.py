f5ebac2870ddd843f5710cd69d196a52a3fd1f94
Brandon Schneider <github@bschneider.email>
2017-01-17 22:58:15 -0600

    Add "copytree" to ioc_common.

--- iocage/lib/ioc_common.py
+++ iocage/lib/ioc_common.py
@@ -1,4 +1,8 @@
 """Common methods we reuse."""
+import shutil
+
+import os
+import stat
 
 
 def sort_tag(tag):
@@ -56,3 +60,32 @@ def sort_release(releases, split=False):
             release_list.insert(index, "{}-RELEASE".format(r))
 
     return release_list
+
+
+# Cyrille Pontvieux on StackOverflow
+def copytree(src, dst, symlinks=False, ignore=None):
+    """Copies a tree and overwrites."""
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+        shutil.copystat(src, dst)
+    lst = os.listdir(src)
+    if ignore:
+        excl = ignore(src, lst)
+        lst = [x for x in lst if x not in excl]
+    for item in lst:
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if symlinks and os.path.islink(s):
+            if os.path.lexists(d):
+                os.remove(d)
+            os.symlink(os.readlink(s), d)
+            try:
+                st = os.lstat(s)
+                mode = stat.S_IMODE(st.st_mode)
+                os.lchmod(d, mode)
+            except:
+                pass  # lchmod not available
+        elif os.path.isdir(s):
+            copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
