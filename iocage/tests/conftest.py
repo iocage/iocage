bc5a0a5cc44b18ba6bfa622d89dfedbd37133222
Brandon Schneider <brandonschneider89@gmail.com>
2017-01-17 23:17:35 -0600

    Add README for tests.

--- iocage/tests/conftest.py
+++ iocage/tests/conftest.py
@@ -9,9 +9,9 @@ def pytest_addoption(parser):
     parser.addoption("--server", action="store", default="ftp.freebsd.org",
                      help="FTP server to login to.")
     parser.addoption("--user", action="store", default="anonymous",
-                     help="The user to use.")
+                     help="The user to use for fetching.")
     parser.addoption("--password", action="store", default="anonymous@",
-                     help="The password to use.")
+                     help="The password to use for fetching.")
     parser.addoption("--http", action="store_true",
                      help="Have --server define a HTTP server instead.")
     parser.addoption("--auth", action="store", default=None,
@@ -25,49 +25,58 @@ def pytest_addoption(parser):
                           "FTP or HTTP.")
     parser.addoption(
             "--root-dir", action="store",
-            help="Root directory containing all the RELEASEs.")
+            help="Root directory containing all the RELEASEs for fetching.")
 
 
 @pytest.fixture
 def zpool(request):
+    """Specify a zpool to use."""
     return request.config.getoption("--zpool")
 
 
 @pytest.fixture
 def release(request):
+    """Specify a RELEASE to use."""
     return request.config.getoption("--release")
 
 
 @pytest.fixture
 def server(request):
+    """FTP server to login to."""
     return request.config.getoption("--server")
 
 
 @pytest.fixture
 def user(request):
+    """The user to use for fetching."""
     return request.config.getoption("--user")
 
 
 @pytest.fixture
 def password(request):
+    """The password to use for fetching."""
     return request.config.getoption("--password")
 
 
 @pytest.fixture
 def root_dir(request):
+    """Root directory containing all the RELEASEs for fetching."""
     return request.config.getoption("--root-dir")
 
 
 @pytest.fixture
 def http(request):
+    """Have --server define a HTTP server instead."""
     return request.config.getoption("--http")
 
 
 @pytest.fixture
 def _file(request):
+    """Use a local file directory for root-dir instead of FTP or HTTP."""
     return request.config.getoption("--file")
 
 
 @pytest.fixture
 def auth(request):
+    """Authentication method for HTTP fetching. Valid values: basic, digest"""
     return request.config.getoption("--auth")
