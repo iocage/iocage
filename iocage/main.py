9e01b66ecd722b12f900d514446ff3476bec71cf
Brandon Schneider <github@bschneider.email>
2017-01-18 21:40:17 -0600

    Bump version to 0.9.1

--- iocage/main.py
+++ iocage/main.py
@@ -16,7 +16,7 @@ def print_version(ctx, param, value):
     """Prints the version and then exits."""
     if not value or ctx.resilient_parsing:
         return
-    print("Version\t0.9 2017/01/16")
+    print("Version\t0.9.1 2017/01/18")
     sys.exit()
 
 
