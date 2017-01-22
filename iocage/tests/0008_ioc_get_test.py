8285c157db031c796db5cea91fbb258a4cdbf38c
Brandon Schneider <github@bschneider.email>
2017-01-21 20:14:08 -0600

    Reformat tests and add test for short_uuid creation and manipulation.

--- iocage/tests/0008_ioc_get_test.py
+++ iocage/tests/0008_ioc_get_test.py
@@ -4,8 +4,12 @@ from iocage.lib.ioc_list import IOCList
 
 def test_get():
     _, paths = IOCList("uuid").get_datasets()
+
     path = paths["newtest"]
+    path_short = paths["newtest_short"]
 
     prop = IOCJson(path).get_prop_value("tag")
+    prop_short = IOCJson(path_short).get_prop_value("tag")
 
     assert prop == "newtest"
+    assert prop_short == "newtest_short"
--- iocage/tests/0009_ioc_destroy_test.py
+++ iocage/tests/0009_ioc_destroy_test.py
@@ -4,9 +4,14 @@ from iocage.lib.ioc_list import IOCList
 
 def test_destroy():
     jails, paths = IOCList("uuid").get_datasets()
+
     uuid = jails["newtest"]
+    uuid_short = jails["newtest_short"]
+
     path = paths["newtest"]
+    path_short = paths["newtest_short"]
 
     IOCDestroy(uuid, "newtest", path).destroy_jail()
+    IOCDestroy(uuid_short, "newtest_short", path_short).destroy_jail()
 
     assert True == True
