36a278983b9063bde7e8e05e7cc8d1a3856df5f7
Brandon Schneider <brandonschneider89@gmail.com>
2017-01-23 23:08:39 -0600

    Let's be atomic!

--- iocage/lib/ioc_common.py
+++ iocage/lib/ioc_common.py
@@ -1,5 +1,7 @@
 """Common methods we reuse."""
 import shutil
+import tempfile as tmp
+from contextlib import contextmanager
 
 import os
 import stat
@@ -90,3 +92,69 @@ def copytree(src, dst, symlinks=False, ignore=None):
             copytree(s, d, symlinks, ignore)
         else:
             shutil.copy2(s, d)
+
+
+# http://stackoverflow.com/questions/2333872/atomic-writing-to-file-with-python
+@contextmanager
+def tempfile(suffix='', dir=None):
+    """
+    Context for temporary file.
+
+    Will find a free temporary filename upon entering
+    and will try to delete the file on leaving, even in case of an exception.
+
+    Parameters
+    ----------
+    suffix : string
+        optional file suffix
+    dir : string
+        optional directory to save temporary file in
+    """
+
+    tf = tmp.NamedTemporaryFile(delete=False, suffix=suffix, dir=dir)
+    tf.file.close()
+    try:
+        yield tf.name
+    finally:
+        try:
+            os.remove(tf.name)
+        except OSError as e:
+            if e.errno == 2:
+                pass
+            else:
+                raise
+
+
+@contextmanager
+def open_atomic(filepath, *args, **kwargs):
+    """
+    Open temporary file object that atomically moves to destination upon
+    exiting.
+
+    Allows reading and writing to and from the same filename.
+
+    The file will not be moved to destination in case of an exception.
+
+    Parameters
+    ----------
+    filepath : string
+        the file path to be opened
+    fsync : bool
+        whether to force write the file to disk
+    *args : mixed
+        Any valid arguments for :code:`open`
+    **kwargs : mixed
+        Any valid keyword arguments for :code:`open`
+    """
+    fsync = kwargs.get('fsync', False)
+
+    with tempfile(dir=os.path.dirname(os.path.abspath(filepath))) as tmppath:
+        with open(tmppath, *args, **kwargs) as file:
+            try:
+                yield file
+            finally:
+                if fsync:
+                    file.flush()
+                    os.fsync(file.fileno())
+        os.rename(tmppath, filepath)
+        os.chmod(filepath, 0o644)
