639e8f7bc396cd8f6e2202c78aee8d495f356c96
Brandon Schneider <brandonschneider89@gmail.com>
2017-01-26 20:05:06 -0600

    Allow specifying multiple jails to `start` and `stop`. Example: `iocage start foo bar ; iocage stop foo bar`

--- iocage/cli/start.py
+++ iocage/cli/start.py
@@ -12,49 +12,51 @@ __rootcmd__ = True
 
 
 @click.command(name="start", help="Starts the specified jails or ALL.")
-@click.argument("jail")
-def start_cmd(jail):
+@click.argument("jails", nargs=-1)
+def start_cmd(jails):
     """
     Looks for the jail supplied and passes the uuid, path and configuration
     location to start_jail.
     """
     lgr = logging.getLogger('ioc_cli_start')
 
-    jails, paths = IOCList("uuid").get_datasets()
-    if jail == "ALL":
-        for j in jails:
-            uuid = jails[j]
+    _jails, paths = IOCList("uuid").get_datasets()
+
+    if jails[0] == "ALL":
+        for j in _jails:
+            uuid = _jails[j]
             path = paths[j]
 
             conf = IOCJson(path).load_json()
             IOCStart(uuid, j, path, conf)
     else:
-        _jail = {tag: uuid for (tag, uuid) in jails.iteritems() if
-                 uuid.startswith(jail) or tag == jail}
+        for jail in jails:
+            _jail = {tag: uuid for (tag, uuid) in _jails.iteritems() if
+                     uuid.startswith(jail) or tag == jail}
 
-        if len(_jail) == 1:
-            tag, uuid = next(_jail.iteritems())
-            path = paths[tag]
-        elif len(_jail) > 1:
-            lgr.error("Multiple jails found for"
-                      " {}:".format(jail))
-            for t, u in sorted(_jail.iteritems()):
-                lgr.error("  {} ({})".format(u, t))
-            raise RuntimeError()
-        else:
-            raise RuntimeError("{} not found!".format(jail))
+            if len(_jail) == 1:
+                tag, uuid = next(_jail.iteritems())
+                path = paths[tag]
+            elif len(_jail) > 1:
+                lgr.error("Multiple jails found for"
+                          " {}:".format(jail))
+                for t, u in sorted(_jail.iteritems()):
+                    lgr.error("  {} ({})".format(u, t))
+                raise RuntimeError()
+            else:
+                raise RuntimeError("{} not found!".format(jail))
 
-        conf = IOCJson(path).load_json()
+            conf = IOCJson(path).load_json()
 
-        if conf["type"] == "jail":
-            IOCStart(uuid, tag, path, conf)
-        elif conf["type"] == "basejail":
-            raise RuntimeError("Please run \"iocage migrate\" before trying"
-                               " to start {} ({})".format(uuid, tag))
-        elif conf["type"] == "template":
-            raise RuntimeError("Please convert back to a jail before trying"
-                               " to start {} ({})".format(uuid, tag))
-        else:
-            raise RuntimeError("{} is not a supported jail type.".format(
-                conf["type"]
-            ))
+            if conf["type"] == "jail":
+                IOCStart(uuid, tag, path, conf)
+            elif conf["type"] == "basejail":
+                raise RuntimeError("Please run \"iocage migrate\" before trying"
+                                   " to start {} ({})".format(uuid, tag))
+            elif conf["type"] == "template":
+                raise RuntimeError("Please convert back to a jail before trying"
+                                   " to start {} ({})".format(uuid, tag))
+            else:
+                raise RuntimeError("{} is not a supported jail type.".format(
+                    conf["type"]
+                ))
--- iocage/cli/stop.py
+++ iocage/cli/stop.py
@@ -12,37 +12,39 @@ __rootcmd__ = True
 
 
 @click.command(name="stop", help="Stops the specified jails or ALL.")
-@click.argument('jail')
-def stop_cmd(jail):
+@click.argument("jails", nargs=-1)
+def stop_cmd(jails):
     """
     Looks for the jail supplied and passes the uuid, path and configuration
     location to stop_jail.
     """
-    lgr = logging.getLogger('ioc_cli_get')
+    lgr = logging.getLogger('ioc_cli_stop')
 
-    jails, paths = IOCList("uuid").get_datasets()
-    if jail == "ALL":
-        for j in jails:
-            uuid = jails[j]
+    _jails, paths = IOCList("uuid").get_datasets()
+
+    if jails[0] == "ALL":
+        for j in _jails:
+            uuid = _jails[j]
             path = paths[j]
 
             conf = IOCJson(path).load_json()
-            IOCStop(uuid, jail, path, conf)
+            IOCStop(uuid, j, path, conf)
     else:
-        _jail = {tag: uuid for (tag, uuid) in jails.iteritems() if
-                 uuid.startswith(jail) or tag == jail}
+        for jail in jails:
+            _jail = {tag: uuid for (tag, uuid) in _jails.iteritems() if
+                     uuid.startswith(jail) or tag == jail}
 
-        if len(_jail) == 1:
-            tag, uuid = next(_jail.iteritems())
-            path = paths[tag]
-        elif len(_jail) > 1:
-            lgr.error("Multiple jails found for"
-                      " {}:".format(jail))
-            for t, u in sorted(_jail.iteritems()):
-                lgr.error("  {} ({})".format(u, t))
-            raise RuntimeError()
-        else:
-            raise RuntimeError("{} not found!".format(jail))
+            if len(_jail) == 1:
+                tag, uuid = next(_jail.iteritems())
+                path = paths[tag]
+            elif len(_jail) > 1:
+                lgr.error("Multiple jails found for"
+                          " {}:".format(jail))
+                for t, u in sorted(_jail.iteritems()):
+                    lgr.error("  {} ({})".format(u, t))
+                raise RuntimeError()
+            else:
+                raise RuntimeError("{} not found!".format(jail))
 
-        conf = IOCJson(path).load_json()
-        IOCStop(uuid, tag, path, conf)
+            conf = IOCJson(path).load_json()
+            IOCStop(uuid, tag, path, conf)
