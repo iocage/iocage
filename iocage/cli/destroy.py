2269fd171571b9506cdef60ddebf91be00921d4a
Brandon Schneider <github@bschneider.email>
2017-03-28 18:35:21 -0500

    Allow `destroy`ing RELEASE. Example: `iocage destroy -r 11.0-RELEASE`

--- iocage/cli/destroy.py
+++ iocage/cli/destroy.py
@@ -12,13 +12,20 @@ __rootcmd__ = True
 
 @click.command(name="destroy", help="Destroy specified jail(s).")
 @click.option("--force", "-f", default=False, is_flag=True)
+@click.option("--release", "-r", default=False, is_flag=True)
+@click.option("--download", "-d", default=False, is_flag=True,
+              help="Delete the download dataset of the specified RELEASE as"
+                   " well.")
 @click.argument("jails", nargs=-1)
-def destroy_cmd(force, jails):
+def destroy_cmd(force, release, download, jails):
     """Destroys the jail's 2 datasets and the snapshot from the RELEASE."""
     lgr = ioc_logger.Logger('ioc_cli_destroy')
     lgr = lgr.getLogger()
 
-    if jails:
+    if download and not release:
+        exit("--release (-r) must be specified as well!")
+
+    if jails and not release:
         get_jid = IOCList().list_get_jid
 
         try:
@@ -73,6 +80,28 @@ def destroy_cmd(force, jails):
                 lgr.info("Stopping {} ({}).".format(uuid, tag))
 
             IOCDestroy().destroy_jail(path)
+    elif jails and release:
+        pool = IOCJson().json_get_value("pool")
+
+        for release in jails:
+            path = f"{pool}/iocage/releases/{release}"
+
+            if not force:
+                lgr.warning(f"\nWARNING: This will destroy RELEASE: {release}")
+                lgr.warning("       and any jail that was created with it.")
+
+                if not click.confirm("\nAre you sure?"):
+                    continue
+
+            IOCDestroy().__destroy_parse_datasets__(path)
+
+            if download:
+                path = f"{pool}/iocage/download/{release}"
+                IOCDestroy().__destroy_parse_datasets__(path)
+
+    elif not jails and release:
+        lgr.critical("Please specify one or more RELEASEs!")
+        exit(1)
     else:
         lgr.critical("Please specify one or more jails!")
         exit(1)
