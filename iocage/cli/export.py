3b5ca5d6f3a76c25b004606c2ccd71cd433695f5
Brandon Schneider <brandonschneider89@gmail.com>
2017-04-05 22:48:57 -0500

    Split out `export` and `import` from the cli.

--- iocage/cli/export.py
+++ iocage/cli/export.py
@@ -1,30 +1,25 @@
 """export module for the cli."""
-import os
-import zipfile
-from datetime import datetime
-from subprocess import CalledProcessError, PIPE, Popen, STDOUT, check_call
-
 import click
 
 import iocage.lib.ioc_logger as ioc_logger
-from iocage.lib.ioc_common import checkoutput
-from iocage.lib.ioc_json import IOCJson
+from iocage.lib.ioc_image import IOCImage
 from iocage.lib.ioc_list import IOCList
 
 __cmdname__ = "export_cmd"
 __rootcmd__ = True
+
+lgr = ioc_logger.Logger("ioc_cli_export")
+lgr = lgr.getLogger()
+
+
+def callback(message):
+    lgr.info(message)
 
 
 @click.command(name="export", help="Exports a specified jail.")
 @click.argument("jail", required=True)
 def export_cmd(jail):
     """Make a recursive snapshot of the jail and export to a file."""
-    lgr = ioc_logger.Logger('ioc_cli_export')
-    lgr = lgr.getLogger()
-
-    pool = IOCJson().json_get_value("pool")
-    iocroot = IOCJson(pool).json_get_value("iocroot")
-    date = datetime.utcnow().strftime("%F")
     jails, paths = IOCList("uuid").list_datasets()
     _jail = {tag: uuid for (tag, uuid) in jails.items() if
              uuid.startswith(jail) or tag == jail}
@@ -48,65 +43,4 @@ def export_cmd(jail):
                      "exporting!".format(uuid, tag))
         exit(1)
 
-    images = "{}/images".format(iocroot)
-    name = "{}_{}".format(uuid, date)
-    image = "{}/{}_{}".format(images, name, tag)
-    image_path = "{}{}".format(pool, path)
-    jail_list = []
-
-    # Looks like foo/iocage/jails/df0ef69a-57b6-4480-b1f8-88f7b6febbdf@BAR
-    target = "{}@ioc-export-{}".format(image_path, date)
-
-    try:
-        checkoutput(["zfs", "snapshot", "-r", target], stderr=STDOUT)
-    except CalledProcessError as err:
-        lgr.critical("{}".format(err.output.decode("utf-8").rstrip()))
-        exit(1)
-
-    datasets = Popen(["zfs", "list", "-H", "-r",
-                      "-o", "name", "{}{}".format(pool, path)],
-                     stdout=PIPE, stderr=PIPE).communicate()[0].decode(
-        "utf-8").split()
-
-    for dataset in datasets:
-        if len(dataset) == 54:
-            _image = image
-            jail_list.append(_image)
-        elif len(dataset) > 54:
-            image_name = dataset.partition("{}{}".format(pool, path))[2]
-            name = image_name.replace("/", "_")
-            _image = image + name
-            jail_list.append(_image)
-            target = "{}@ioc-export-{}".format(dataset, date)
-
-        # Sending each individually as sending them recursively to a file does
-        # not work how one expects.
-        try:
-            with open(_image, "wb") as export:
-                lgr.info("Exporting dataset: {}".format(dataset))
-                check_call(["zfs", "send", target], stdout=export)
-        except CalledProcessError as err:
-            lgr.critical("{}".format(err))
-            exit(1)
-
-    lgr.info("\nPreparing zip file: {}.zip.".format(image))
-    with zipfile.ZipFile("{}.zip".format(image), "w",
-                         compression=zipfile.ZIP_DEFLATED,
-                         allowZip64=True) as final:
-        os.chdir(images)
-
-        for jail in jail_list:
-            final.write(jail)
-
-    # Cleanup our mess.
-    try:
-        checkoutput(["zfs", "destroy", "-r", target], stderr=STDOUT)
-
-        for jail in jail_list:
-            os.remove(jail)
-
-    except CalledProcessError as err:
-        lgr.critical("{}".format(err.output.decode("utf-8").rstrip()))
-        exit(1)
-
-    lgr.info("\nExported: {}.zip".format(image))
+    IOCImage(callback=callback).export_jail(uuid, tag, path)
---
+++ iocage/lib/ioc_image.py
@@ -0,0 +1,152 @@
+"""iocage export and import module"""
+import fnmatch
+import os
+import zipfile
+from datetime import datetime
+from subprocess import CalledProcessError, PIPE, Popen, STDOUT, check_call
+
+import logging
+
+from iocage.lib.ioc_common import checkoutput
+from iocage.lib.ioc_json import IOCJson
+
+
+class IOCImage(object):
+    """export() and import()"""
+
+    def __init__(self, callback=None):
+        self.callback = callback
+        self.pool = IOCJson().json_get_value("pool")
+        self.iocroot = IOCJson(self.pool).json_get_value("iocroot")
+        self.date = datetime.utcnow().strftime("%F")
+
+    def export_jail(self, uuid, tag, path):
+        """Make a recursive snapshot of the jail and export to a file."""
+        images = f"{self.iocroot}/images"
+        name = f"{uuid}_{self.date}"
+        image = f"{images}/{name}_{tag}"
+        image_path = f"{self.pool}{path}"
+        jail_list = []
+
+        # Looks like foo/iocage/jails/df0ef69a-57b6-4480-b1f8-88f7b6febbdf@BAR
+        target = f"{image_path}@ioc-export-{self.date}"
+
+        try:
+            checkoutput(["zfs", "snapshot", "-r", target], stderr=STDOUT)
+        except CalledProcessError as err:
+            raise RuntimeError(f"{err.output.decode('utf-8').rstrip()}")
+
+        datasets = Popen(["zfs", "list", "-H", "-r",
+                          "-o", "name", f"{self.pool}{path}"],
+                         stdout=PIPE, stderr=PIPE).communicate()[0].decode(
+            "utf-8").split()
+
+        for dataset in datasets:
+            if len(dataset) == 54:
+                _image = image
+                jail_list.append(_image)
+            elif len(dataset) > 54:
+                image_name = dataset.partition(f"{self.pool}{path}")[2]
+                name = image_name.replace("/", "_")
+                _image = image + name
+                jail_list.append(_image)
+                target = f"{dataset}@ioc-export-{self.date}"
+
+            # Sending each individually as sending them recursively to a file
+            # does not work how one expects.
+            try:
+                with open(_image, "wb") as export:
+                    if callable(self.callback):
+                        self.callback(f"Exporting dataset: {dataset}")
+
+                    check_call(["zfs", "send", target], stdout=export)
+            except CalledProcessError as err:
+                raise RuntimeError(err)
+
+        if callable(self.callback):
+            self.callback(f"\nPreparing zip file: {image}.zip.")
+
+        with zipfile.ZipFile(f"{image}.zip", "w",
+                             compression=zipfile.ZIP_DEFLATED,
+                             allowZip64=True) as final:
+            os.chdir(images)
+
+            for jail in jail_list:
+                final.write(jail)
+
+        # Cleanup our mess.
+        try:
+            checkoutput(["zfs", "destroy", "-r", target], stderr=STDOUT)
+
+            for jail in jail_list:
+                os.remove(jail)
+
+        except CalledProcessError as err:
+            raise RuntimeError(f"{err.output.decode('utf-8').rstrip()}")
+
+        if callable(self.callback):
+            self.callback("\nExported: {}.zip".format(image))
+
+    def import_jail(self, jail):
+        """Import from an iocage export."""
+        image_dir = f"{self.iocroot}/images"
+        exports = os.listdir(image_dir)
+        uuid_matches = fnmatch.filter(exports, f"{jail}*.zip")
+        tag_matches = fnmatch.filter(exports, f"*{jail}.zip")
+        cmd = ["zfs", "recv", "-F", "-d", self.pool]
+
+        # We want to allow the user some flexibility.
+        if uuid_matches:
+            matches = uuid_matches
+        else:
+            matches = tag_matches
+
+        if len(matches) == 1:
+            image_target = f"{image_dir}/{matches[0]}"
+            uuid = matches[0].rsplit("_")[0]
+            date = matches[0].rsplit("_")[1]
+            tag = matches[0].rsplit("_")[2].rsplit(".")[0]
+        elif len(matches) > 1:
+            msg = f"Multiple exports found for {jail}:"
+
+            for j in sorted(matches):
+                msg += f"\n  {j}"
+
+            raise RuntimeError(msg)
+        else:
+            raise RuntimeError(f"{jail} not found!")
+
+        with zipfile.ZipFile(image_target, "r") as _import:
+            for z in _import.namelist():
+                z_split = z.split("_")
+
+                # We don't want the date and tag
+                del z_split[1]
+                del z_split[1]
+
+                z_split_str = "/".join(z_split)
+                _z = z_split_str.replace("iocage/images/", "")
+
+                if callable(self.callback):
+                    self.callback("Importing dataset: {}".format(_z))
+
+                dataset = _import.read(z)
+                recv = Popen(cmd, stdin=PIPE)
+                recv.stdin.write(dataset)
+                recv.communicate()
+                recv.stdin.close()
+
+        # Cleanup our mess.
+        try:
+            target = f"{self.pool}{self.iocroot}/jails/{uuid}@ioc-export-" \
+                     f"{date}"
+
+            checkoutput(["zfs", "destroy", "-r", target], stderr=STDOUT)
+        except CalledProcessError as err:
+            raise RuntimeError(f"{err.output.decode('utf-8').rstrip()}")
+
+        tag = IOCJson(f"{self.iocroot}/jails/{uuid}",
+                      silent=True).json_set_value(f"tag={tag}")
+
+        if callable(self.callback):
+            self.callback(f"\nImported: {uuid} ({tag})")
