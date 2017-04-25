86ec85ac947e6f280eb55fe7240a40a92414a748
Brandon Schneider <github@bschneider.email>
2017-04-25 13:02:07 -0500

    Add compressed man page and install it with setup.py

--- setup.py
+++ setup.py
@@ -5,15 +5,17 @@ import fastentrypoints
 from setuptools import find_packages, setup
 
 if os.path.isdir("/usr/local/etc/init.d"):
-    _data = [('/usr/local/etc/init.d', ['rc.d/iocage'])]
+    _data = [('/usr/local/etc/init.d', ['rc.d/iocage']),
+             ('/usr/local/man/man8', ['iocage/iocage.8.gz'])]
 else:
-    _data = [('/usr/local/etc/rc.d', ['rc.d/iocage'])]
+    _data = [('/usr/local/etc/rc.d', ['rc.d/iocage']),
+             ('/usr/local/man/man8', ['iocage/iocage.8.gz'])]
 
 if sys.version_info < (3, 6):
     exit("Only Python 3.6 and higher is supported.")
 
 setup(name='iocage',
-      version='0.9.7',
+      version='0.9.8b',
       description='A jail manager that uses ZFS.',
       author='Brandon Schneider and Peter Toth',
       author_email='brandon@ixsystems.com',
