5bc8aec4e37fab780bd83d43abfb04fcefbb2828
Brandon Schneider <brandonschneider89@gmail.com>
2017-04-27 06:20:22 -0500

    Fix bad `start` and `stop` tests.

--- iocage/tests/functional_tests/0005_ioc_start_test.py
+++ iocage/tests/functional_tests/0005_ioc_start_test.py
@@ -9,6 +9,7 @@ require_zpool = pytest.mark.require_zpool
 @require_root
 @require_zpool
 def test_start():
-    IOCage(["test", "test_short"]).start()
+    IOCage("test").start()
+    IOCage("test_short").start()
 
     assert True == True
--- iocage/tests/functional_tests/0006_ioc_stop_test.py
+++ iocage/tests/functional_tests/0006_ioc_stop_test.py
@@ -9,6 +9,7 @@ require_zpool = pytest.mark.require_zpool
 @require_root
 @require_zpool
 def test_stop():
-    IOCage(["test", "test_short"]).stop()
+    IOCage("test").stop()
+    IOCage("test_short").stop()
 
     assert True == True
