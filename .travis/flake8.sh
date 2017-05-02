0dfe8211567f77166bee92a9d552fd9032d451d9
Brandon Schneider <github@bschneider.email>
2017-05-02 10:11:45 -0500

    Add flake8 script for travis, one more attempt to make that thing useful for us. Unashamedly ripped off @william-gr script at https://github.com/freenas/freenas/blob/master/.travis/flake8.sh :D

---
+++ .travis/flake8.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -x
+
+# Run pep8 on all .py files in all subfolders
+
+tmpafter=$(mktemp)
+find ~/iocage/iocage/cli ~/iocage/iocage/lib -name \*.py -exec flake8 {} + > $tmpafter
+num_errors_after=`cat $tmpafter | wc -l`
+echo $num_errors_after
+
+git checkout HEAD~
+
+tmpbefore=$(mktemp)
+find ~/iocage/iocage/cli ~/iocage/iocage/lib -name \*.py -exec flake8 {} + > $tmpbefore
+num_errors_before=`cat $tmpbefore | wc -l`
+echo $num_errors_before
+
+
+if [ $num_errors_after -gt $num_errors_before ]; then
+	echo "New Flake8 errors were introduced:"
+	diff -u $tmpbefore $tmpafter
+	exit 1
+fi
