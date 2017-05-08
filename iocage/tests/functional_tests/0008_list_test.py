3a1208d13361c6b4c67b37fee0bc75f7269db936
Brandon Schneider <github@bschneider.email>
2017-05-08 14:53:51 -0500

    Add `list` test.

---
+++ iocage/tests/functional_tests/0008_list_test.py
@@ -0,0 +1,42 @@
+import pytest
+from click.testing import CliRunner
+
+from iocage import main as ioc
+
+require_root = pytest.mark.require_root
+require_zpool = pytest.mark.require_zpool
+
+
+@require_zpool
+def test_list(release):
+    runner = CliRunner()
+
+    result_output = \
+        '+-----+----------+-------+---------------+--------------+-----+\n| ' \
+        'JID |   UUID   | STATE |      TAG      |   RELEASE    | IP4 ' \
+        '|\n+=====+==========+=======+===============+==============+=====+' \
+        f'\n| -   | 771ec0cf | down  | newtest       | {release} | -   |' \
+        '\n+-----+----------+-------+---------------+--------------+-----+\n' \
+        f'| -   | dfb013e5 | down  | newtest_short | {release} | -   |\n' \
+        '+-----+----------+-------+---------------+--------------+-----+\n'
+    result_noheader_output = f'-\t771ec0cf\tdown\tnewtest\t{release}\t-\n' \
+                             f'-\tdfb013e5\tdown\tnewtest_short\t{release}' \
+                             '\t-\n'
+    result_release_header_output = \
+        '+---------------+\n| Bases fetched |\n+===============+\n' \
+        f'| {release}  |\n+---------------+\n'
+    result_release_noheader_output = f'{release}\n'
+
+    result_header = runner.invoke(ioc.cli, ["list"])
+    result_noheader = runner.invoke(ioc.cli, ["list", "-H"])
+    result_release_header = runner.invoke(ioc.cli, ["list", "-r"])
+    result_release_noheader = runner.invoke(ioc.cli, ["list", "-r", "-H"])
+    assert result_header.exit_code == 0
+    assert result_release_header.exit_code == 0
+    assert result_noheader.exit_code == 0
+    assert result_release_noheader.exit_code == 0
+
+    assert result_header.output == result_output
+    assert result_release_header.output == result_release_header_output
+    assert result_noheader.output == result_noheader_output
+    assert result_release_noheader.output == result_release_noheader_output
