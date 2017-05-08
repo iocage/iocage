318299ff33a97b0f383dbbbda6df4badc9e037ea
Brandon Schneider <brandonschneider89@gmail.com>
2017-05-08 15:15:46 -0500

    Refactor the `list` test for HardenedBSD.

--- iocage/tests/functional_tests/0008_list_test.py
+++ iocage/tests/functional_tests/0008_list_test.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 from click.testing import CliRunner
 
@@ -8,23 +10,44 @@ require_zpool = pytest.mark.require_zpool
 
 
 @require_zpool
-def test_list(release):
+def test_list(release, hardened):
     runner = CliRunner()
 
-    result_output = \
-        '+-----+----------+-------+---------------+--------------+-----+\n| ' \
-        'JID |   UUID   | STATE |      TAG      |   RELEASE    | IP4 ' \
-        '|\n+=====+==========+=======+===============+==============+=====+' \
-        f'\n| -   | 771ec0cf | down  | newtest       | {release} | -   |' \
-        '\n+-----+----------+-------+---------------+--------------+-----+\n' \
-        f'| -   | dfb013e5 | down  | newtest_short | {release} | -   |\n' \
-        '+-----+----------+-------+---------------+--------------+-----+\n'
+    if hardened:
+        release = release.replace("-RELEASE", "-STABLE")
+        release = re.sub(r"\W\w.", "-", release)
+
+    if hardened:
+        result_output = \
+            '+-----+----------+-------+---------------+-----------+-----+\n' \
+            '| JID |   UUID   | STATE |      TAG      |  RELEASE  | IP4 ' \
+            '|\n+=====+==========+=======+===============+===========+=====+' \
+            f'\n| -   | 771ec0cf | down  | newtest       | {release} | -   |' \
+            '\n+-----+----------+-------+---------------+-----------+-----+' \
+            f'\n| -   | dfb013e5 | down  | newtest_short | {release} | -   |' \
+            '\n+-----+----------+-------+---------------+-----------+-----+\n'
+
+        result_release_header_output = \
+            '+---------------+\n| Bases fetched ' \
+            f'|\n+===============+\n| {release}     |\n+---------------+\n'
+    else:
+        result_output = \
+            '+-----+----------+-------+---------------+--------------+-----+' \
+            '\n| JID |   UUID   | STATE |      TAG      |   RELEASE    | IP4' \
+            ' |\n+=====+==========+=======+===============+==============+' \
+            f'=====+\n| -   | 771ec0cf | down  | newtest       | {release} |' \
+            ' -   |\n+-----+----------+-------+---------------+-------------' \
+            f'-+-----+\n| -   | dfb013e5 | down  | newtest_short | {release}' \
+            ' | -   |\n+-----+----------+-------+---------------+-----------' \
+            '---+-----+\n'
+
+        result_release_header_output = \
+            '+---------------+\n| Bases fetched |\n+===============+\n' \
+            f'| {release}  |\n+---------------+\n'
+
     result_noheader_output = f'-\t771ec0cf\tdown\tnewtest\t{release}\t-\n' \
                              f'-\tdfb013e5\tdown\tnewtest_short\t{release}' \
                              '\t-\n'
-    result_release_header_output = \
-        '+---------------+\n| Bases fetched |\n+===============+\n' \
-        f'| {release}  |\n+---------------+\n'
     result_release_noheader_output = f'{release}\n'
 
     result_header = runner.invoke(ioc.cli, ["list"])
