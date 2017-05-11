822e96b93a2d4a9b4a7860c64fc757a1b18b2f6c
Brandon Schneider <github@bschneider.email>
2017-05-11 18:27:06 -0500

    Fix clean not removing IP addresses. SImplify clean code as well.

--- iocage/lib/ioc_clean.py
+++ iocage/lib/ioc_clean.py
@@ -1,6 +1,6 @@
 """Destroy all of a dataset type."""
-import libzfs
 
+import iocage.lib.ioc_common
 import iocage.lib.ioc_destroy
 import iocage.lib.ioc_json
 
@@ -8,41 +8,50 @@ import iocage.lib.ioc_json
 class IOCClean(object):
     """Cleans datasets and snapshots of a given type."""
 
-    def __init__(self):
+    def __init__(self, callback=None, silent=False):
         self.pool = iocage.lib.ioc_json.IOCJson().json_get_value("pool")
+        self.callback = callback
+        self.silent = silent
 
     def clean_jails(self):
         """Cleans all jails and their respective snapshots."""
+        iocage.lib.ioc_common.logit({
+            "level"  : "INFO",
+            "message": "Cleaning iocage/jails"
+        },
+            _callback=self.callback,
+            silent=self.silent)
+
         iocage.lib.ioc_destroy.IOCDestroy().destroy_jail(
             f"{self.pool}/iocage/jails",
             clean=True)
 
     def clean_all(self):
         """Cleans everything related to iocage."""
-        iocage.lib.ioc_destroy.IOCDestroy().__stop_jails__()
+        datasets = ("iocage", "iocage/download", "iocage/images",
+                    "iocage/jails", "iocage/log", "iocage/releases",
+                    "iocage/templates")
 
-        zfs = libzfs.ZFS(history=True, history_prefix="<iocage>")
-        datasets = zfs.get_dataset(f"{self.pool}/iocage")
+        for dataset in reversed(datasets):
+            iocage.lib.ioc_common.logit({
+                "level"  : "INFO",
+                "message": f"Cleaning {dataset}"
+            },
+                _callback=self.callback,
+                silent=self.silent)
 
-        for dataset in datasets.dependents:
-            try:
-                if dataset.type == libzfs.DatasetType.FILESYSTEM:
-                    dataset.umount(force=True)
-            except libzfs.ZFSException as err:
-                # This is either not mounted or doesn't exist anymore,
-                # we don't care either way.
-                if err.code == libzfs.Error.NOENT:
-                    continue
-                else:
-                    raise
-
-            dataset.delete()
-
-        datasets.umount(force=True)
-        datasets.delete()
+            iocage.lib.ioc_destroy.IOCDestroy().__destroy_parse_datasets__(
+                f"{self.pool}/{dataset}", clean=True)
 
     def clean_templates(self):
         """Cleans all templates and their respective children."""
+        iocage.lib.ioc_common.logit({
+            "level"  : "INFO",
+            "message": "Cleaning iocage/templates"
+        },
+            _callback=self.callback,
+            silent=self.silent)
+
         iocage.lib.ioc_destroy.IOCDestroy().__destroy_parse_datasets__(
             f"{self.pool}/iocage/templates",
             clean=True)
