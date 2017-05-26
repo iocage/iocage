863e24d778c9de23fdcdffc0cc5c315fd2b10294
Brandon Schneider <brandon@ixsystems.com>
2017-05-25 22:17:02 -0500

    Rewrite `df` to use libzfs, fold into API

--- iocage/cli/df.py
+++ iocage/cli/df.py
@@ -1,12 +1,10 @@
 """df module for the cli."""
-import subprocess as su
 
 import click
 import texttable
 
 import iocage.lib.ioc_common as ioc_common
-import iocage.lib.ioc_json as ioc_json
-import iocage.lib.ioc_list as ioc_list
+import iocage.lib.iocage as ioc
 
 
 @click.command(name="df", help="Show resource usage of all jails.")
@@ -18,48 +16,8 @@ import iocage.lib.ioc_list as ioc_list
               help="Sorts the list by the given type")
 def cli(header, _long, _sort):
     """Allows a user to show resource usage of all jails."""
-    jails, paths = ioc_list.IOCList("uuid").list_datasets()
-    pool = ioc_json.IOCJson().json_get_value("pool")
-    jail_list = []
     table = texttable.Texttable(max_width=0)
-
-    for jail in jails:
-        full_uuid = jails[jail]
-
-        if not _long:
-            uuid = full_uuid[:8]
-        else:
-            uuid = full_uuid
-
-        path = paths[jail]
-        conf = ioc_json.IOCJson(path).json_load()
-        zconf = ["zfs", "get", "-H", "-o", "value"]
-        mountpoint = f"{pool}/iocage/jails/{full_uuid}"
-
-        tag = conf["tag"]
-        template = conf["type"]
-
-        if template == "template":
-            mountpoint = f"{pool}/iocage/templates/{tag}"
-
-        compressratio = su.Popen(zconf + ["compressratio", mountpoint],
-                                 stdout=su.PIPE).communicate()[0].decode(
-            "utf-8").strip()
-        reservation = su.Popen(zconf + ["reservation", mountpoint],
-                               stdout=su.PIPE).communicate()[0].decode(
-            "utf-8").strip()
-        quota = su.Popen(zconf + ["quota", mountpoint],
-                         stdout=su.PIPE).communicate()[0].decode(
-            "utf-8").strip()
-        used = su.Popen(zconf + ["used", mountpoint],
-                        stdout=su.PIPE).communicate()[0].decode(
-            "utf-8").strip()
-        available = su.Popen(zconf + ["available", mountpoint],
-                             stdout=su.PIPE).communicate()[0].decode(
-            "utf-8").strip()
-
-        jail_list.append([uuid, compressratio, reservation, quota, used,
-                          available, tag])
+    jail_list = ioc.IOCage().df(long=_long)
 
     sort = ioc_common.ioc_sort("df", _sort)
     jail_list.sort(key=sort)
