65922fe9794495b2b849464e2edef202eb711547
Brandon Schneider <brandon@ixsystems.com>
2017-05-28 23:33:23 -0500

    Make flake8 script more useful. We don’t want the next commit to be OK because it has the same number of errors as the previous. It’s still a regression.

--- .travis/flake8.sh
+++ .travis/flake8.sh
@@ -7,14 +7,16 @@ set -x
 tmpafter=$(mktemp)
 find ./iocage/cli ./iocage/lib -name \*.py -exec flake8 --ignore=E127,E203 {} + > $tmpafter
 num_errors_after=`cat $tmpafter | wc -l`
-echo $num_errors_after
+echo "Current Error Count: ${num_errors_after}"
 
-git checkout HEAD~
+last_release=$(git describe --tags)
+echo "Comparing with last stable release: ${last_release}"
+git checkout ${last_release}
 
 tmpbefore=$(mktemp)
 find ./iocage/cli ./iocage/lib -name \*.py -exec flake8 --ignore=E127,E203 {} + > $tmpbefore
 num_errors_before=`cat $tmpbefore | wc -l`
-echo $num_errors_before
+echo "${last_release}'s Error Count: ${num_errors_before}"
 
 
 if [ $num_errors_after -gt $num_errors_before ]; then
