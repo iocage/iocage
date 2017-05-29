e81d304672b00ed09c774cf6f91e03d3899e2266
Brandon Schneider <brandon@ixsystems.com>
2017-05-29 00:02:00 -0500

    Oh shell….my old friend. I did not miss you.

--- .travis/flake8.sh
+++ .travis/flake8.sh
@@ -2,8 +2,8 @@
 # Run pep8 on all .py files in all subfolders
 
 tmpafter=$(mktemp)
-find ./iocage/cli ./iocage/lib -name \*.py -exec flake8 --ignore=E127,E203,W503 {} + > $tmpafter
-num_errors_after=`cat $tmpafter | wc -l`
+find ./iocage/cli ./iocage/lib -name \*.py -exec flake8 --ignore=E127,E203,W503 {} + > ${tmpafter}
+num_errors_after=`cat ${tmpafter} | wc -l`
 echo "Current Error Count: ${num_errors_after}"
 
 # Get new tags from remote
@@ -15,15 +15,15 @@ echo "Comparing with last stable release: ${last_release}"
 git checkout ${last_release}
 
 tmpbefore=$(mktemp)
-find ./iocage/cli ./iocage/lib -name \*.py -exec flake8 --ignore=E127,E203,W503 {} + > $tmpbefore
-num_errors_before=`cat $tmpbefore | wc -l`
+find ./iocage/cli ./iocage/lib -name \*.py -exec flake8 --ignore=E127,E203,W503 {} + > ${tmpbefore}
+num_errors_before=`cat ${tmpbefore} | wc -l`
 echo "${last_release}'s Error Count: ${num_errors_before}"
 
 # The number may be lower then the last release, but that doesn't tell them that they're not higher than they should be.
-num_errors_adjusted=${num_errors_before}-${num_error_after}
+num_errors_adjusted=$((num_errors_before-num_errors_after))
 
-if [ $num_errors_adjusted != 0 ]; then
+if [ ${num_errors_adjusted} != 0 ] && [ ${num_errors_adjusted} != ${num_errors_before} ]; then
 	echo "New Flake8 errors were introduced:"
-	diff -u $tmpbefore $tmpafter
+	diff -u ${tmpbefore} ${tmpafter}
 	exit 1
 fi
