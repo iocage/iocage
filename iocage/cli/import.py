594db96d38e6fff0a2b2598f0b72580268ceb152
Brandon Schneider <brandon@ixsystems.com>
2017-06-08 13:38:37 -0400

    Fix flake8 stuff, why these had extra whitespace and all the others didn’t we may never know.

--- iocage/cli/import.py
+++ iocage/cli/import.py
@@ -1,6 +1,6 @@
 # Copyright (c) 2014-2017, iocage
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted providing that the following conditions
 # are met:
@@ -9,7 +9,7 @@
 # 2. Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
 # IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
 # WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
