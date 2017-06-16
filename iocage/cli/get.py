39a4a772761742ec52ddfa8ffa0bb5c7ddb8e96f
Brandon Schneider <brandon@ixsystems.com>
2017-06-15 20:32:28 -0500

    Fold `get` into API

--- iocage/cli/get.py
+++ iocage/cli/get.py
@@ -22,14 +22,11 @@
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 """get module for the cli."""
-import json
-
 import click
 import texttable
 
 import iocage.lib.ioc_common as ioc_common
-import iocage.lib.ioc_json as ioc_json
-import iocage.lib.ioc_list as ioc_list
+import iocage.lib.iocage as ioc
 
 
 @click.command(context_settings=dict(
@@ -51,9 +48,6 @@ import iocage.lib.ioc_list as ioc_list
                                             "zpool.", is_flag=True)
 def cli(prop, _all, _pool, jail, recursive, header, plugin):
     """Get a list of jails and print the property."""
-    get_jid = ioc_list.IOCList.list_get_jid
-    jails, paths = ioc_list.IOCList("uuid").list_datasets()
-    jail_list = []
     table = texttable.Texttable(max_width=0)
 
     if _all:
@@ -61,76 +55,23 @@ def cli(prop, _all, _pool, jail, recursive, header, plugin):
         jail = prop
         prop = "all"
 
-    if _pool:
-        pool = ioc_json.IOCJson().json_get_value("pool")
-
-        ioc_common.logit({
-            "level"  : "INFO",
-            "message": pool
-        })
-        exit()
-
-    if recursive is None:
-        if jail == "":
-            ioc_common.logit({
-                "level"  : "ERROR",
-                "message": 'Usage: iocage get [OPTIONS] PROP JAIL\n'
-                           'Missing argument "jail".'
-            })
-            exit(1)
-
-        _jail = {tag: uuid for (tag, uuid) in jails.items() if
-                 uuid.startswith(jail) or tag == jail}
-
-        if len(_jail) == 1:
-            tag, uuid = next(iter(_jail.items()))
-            path = paths[tag]
-        elif len(_jail) > 1:
-            ioc_common.logit({
-                "level"  : "ERROR",
-                "message": f"Multiple jails found for {jail}:"
-            })
-            for t, u in sorted(_jail.items()):
-                ioc_common.logit({
-                    "level"  : "ERROR",
-                    "message": f"  {u} ({t})"
-                })
-            exit(1)
-        else:
-            ioc_common.logit({
-                "level"  : "ERROR",
-                "message": f"{jail} not found!"
-            })
-            exit(1)
-
+    if not recursive:
         if prop == "state":
-            status, _ = get_jid(path.split("/")[3])
-
-            if status:
-                state = "up"
-            else:
-                state = "down"
+            state = ioc.IOCage(jail).get(prop)
 
             ioc_common.logit({
                 "level"  : "INFO",
                 "message": state
             })
         elif plugin:
-            _prop = prop.split(".")
-            props = ioc_json.IOCJson(path).json_plugin_get_value(_prop)
+            _plugin = ioc.IOCage(jail).get(prop, plugin=True)
 
-            if isinstance(props, dict):
-                ioc_common.logit({
-                    "level"  : "INFO",
-                    "message": json.dumps(props, indent=4)
-                })
-            else:
-                ioc_common.logit({
-                    "level"  : "INFO",
-                    "message": props[0].decode("utf-8")
-                })
+            ioc_common.logit({
+                "level"  : "INFO",
+                "message": _plugin
+            })
         elif prop == "all":
-            props = ioc_json.IOCJson(path).json_get_value(prop)
+            props = ioc.IOCage(jail).get(prop)
 
             for p, v in props.items():
                 ioc_common.logit({
@@ -138,71 +79,32 @@ def cli(prop, _all, _pool, jail, recursive, header, plugin):
                     "message": f"{p}:{v}"
                 })
         elif prop == "fstab":
-            pool = ioc_json.IOCJson().json_get_value("pool")
-            iocroot = ioc_json.IOCJson(pool).json_get_value("iocroot")
-            index = 0
-
-            with open(f"{iocroot}/jails/{uuid}/fstab", "r") as fstab:
-                for line in fstab.readlines():
-                    line = line.rsplit("#")[0].rstrip()
-                    jail_list.append([index, line.replace("\t", " ")])
-                    index += 1
+            fstab_list = ioc.IOCage(jail).get(prop)
 
             if header:
-                jail_list.insert(0, ["INDEX", "FSTAB ENTRY"])
+                fstab_list.insert(0, ["INDEX", "FSTAB ENTRY"])
                 # We get an infinite float otherwise.
                 table.set_cols_dtype(["t", "t"])
-                table.add_rows(jail_list)
+                table.add_rows(fstab_list)
                 ioc_common.logit({
                     "level"  : "INFO",
                     "message": table.draw()
                 })
             else:
-                for fstab in jail_list:
+                for fstab in fstab_list:
                     ioc_common.logit({
                         "level"  : "INFO",
                         "message": f"{fstab[0]}\t{fstab[1]}"
                     })
         else:
-            try:
-                ioc_common.logit({
-                    "level"  : "INFO",
-                    "message": ioc_json.IOCJson(path).json_get_value(prop)
-                })
-            except:
-                ioc_common.logit({
-                    "level"  : "ERROR",
-                    "message": f"{prop} is not a valid property!"
-                })
-                exit(1)
+            p = ioc.IOCage(jail).get(prop)
+
+            ioc_common.logit({
+                "level"  : "INFO",
+                "message": p
+            })
     else:
-        for j in jails:
-            uuid = jails[j]
-            path = paths[j]
-            try:
-                if prop == "state":
-                    status, _ = get_jid(path.split("/")[3])
-
-                    if status:
-                        state = "up"
-                    else:
-                        state = "down"
-
-                    jail_list.append([uuid, j, state])
-                elif prop == "all":
-                    props = ioc_json.IOCJson(path).json_get_value(prop)
-
-                    for p, v in props.items():
-                        jail_list.append([uuid, j, f"{p}:{v}"])
-                else:
-                    jail_list.append(
-                        [uuid, j, ioc_json.IOCJson(path).json_get_value(prop)])
-            except:
-                ioc_common.logit({
-                    "level"  : "ERROR",
-                    "message": f"{prop} is not a valid property!"
-                })
-                exit(1)
+        jail_list = ioc.IOCage().get(prop, recursive=True)
 
         # Prints the table
         if header:
