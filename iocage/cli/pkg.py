fce2604cf411868d911779959d5019da08130f89
Brandon Schneider <github@bschneider.email>
2017-06-16 20:02:53 -0500

    Use API for `pkg`

--- iocage/cli/pkg.py
+++ iocage/cli/pkg.py
@@ -24,9 +24,7 @@
 """pkg module for the cli."""
 import click
 
-import iocage.lib.ioc_common as ioc_common
-import iocage.lib.ioc_exec as ioc_exec
-import iocage.lib.ioc_list as ioc_list
+import iocage.lib.iocage as ioc
 
 __rootcmd__ = True
 
@@ -36,31 +34,5 @@ __rootcmd__ = True
 @click.argument("command", nargs=-1, type=click.UNPROCESSED)
 def cli(command, jail):
     """Runs pkg with the command given inside the specified jail."""
-    jails, paths = ioc_list.IOCList("uuid").list_datasets()
-    _jail = {tag: uuid for (tag, uuid) in jails.items() if
-             uuid.startswith(jail) or tag == jail}
-
-    if len(_jail) == 1:
-        tag, uuid = next(iter(_jail.items()))
-        path = paths[tag]
-    elif len(_jail) > 1:
-        ioc_common.logit({
-            "level"  : "ERROR",
-            "message": f"Multiple jails found for {jail}:"
-        })
-        for t, u in sorted(_jail.items()):
-            ioc_common.logit({
-                "level"  : "ERROR",
-                "message": f"  {u} ({t})"
-            })
-        exit(1)
-    else:
-        ioc_common.logit({
-            "level"  : "ERROR",
-            "message": f"{jail} not found!"
-        })
-        exit(1)
-
     cmd = ("pkg",) + command
-
-    ioc_exec.IOCExec(cmd, uuid, tag, path).exec_jail()
+    ioc.IOCage(jail).exec(cmd)
