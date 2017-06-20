5e853fde316b3379e163a3978e9f0b0553dc80a9
Brandon Schneider <brandon@ixsystems.com>
2017-06-19 20:23:14 -0500

    Whitespace!

--- iocage/cli/rollback.py
+++ iocage/cli/rollback.py
@@ -22,7 +22,6 @@
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 """rollback module for the cli."""
-
 import click
 
 import iocage.lib.ioc_common as ioc_common
