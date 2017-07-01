f7c55992c62d45afa8dd829d33cc2feff58c252e
Brandon Schneider <brandon@ixsystems.com>
2017-06-30 22:00:08 -0500

    API CHANGE: Fold `destroy` into API
    Before it was more of a stub. Everything changed, it is now "destroy_jail" and "destroy_release".

--- iocage/cli/destroy.py
+++ iocage/cli/destroy.py
@@ -41,8 +41,6 @@ __rootcmd__ = True
 @click.argument("jails", nargs=-1)
 def cli(force, release, download, jails):
     """Destroys the jail's 2 datasets and the snapshot from the RELEASE."""
-    pool = ioc.PoolAndDataset().get_pool()
-    iocage = ioc.IOCage(skip_jails=True)
 
     if download and not release:
         ioc_common.logit({
@@ -51,97 +49,19 @@ def cli(force, release, download, jails):
         })
 
     if jails and not release:
-        try:
-            jail_list, paths = ioc.IOCage.list("uuid")
-        except RuntimeError as err:
-            err = str(err)
-
-            if "Configuration is missing" in err:
-                uuid = err.split()[5]
-                path = f"{pool}/iocage/jails/{uuid}"
-
-                if uuid == jails[0]:
-                    iocage.destroy(path, parse=True)
-                    exit()
-                else:
-                    ioc_common.logit({
-                        "level"  : "EXCEPTION",
-                        "message": err
-                    })
-            else:
-                ioc_common.logit({
-                    "level"  : "EXCEPTION",
-                    "message": err
-                })
-        except FileNotFoundError as err:
-            # Jail is lacking a configuration, time to nuke it from orbit.
-            uuid = str(err).rsplit("/")[-2]
-            path = f"{pool}/iocage/jails/{uuid}"
-
-            if uuid == jails[0]:
-                iocage.destroy(path, parse=True)
-                exit()
-            else:
-                ioc_common.logit({
-                    "level"  : "EXCEPTION",
-                    "message": err
-                })
-
         for jail in jails:
-            _jail = {tag: uuid for (tag, uuid) in jail_list.items() if
-                     uuid.startswith(jail) or tag == jail}
-
-            if len(_jail) == 1:
-                tag, uuid = next(iter(_jail.items()))
-                path = paths[tag]
-            elif len(_jail) > 1:
-                ioc_common.logit({
-                    "level"  : "ERROR",
-                    "message": f"Multiple jails found for {jail}:"
-                })
-                for t, u in sorted(_jail.items()):
-                    ioc_common.logit({
-                        "level"  : "ERROR",
-                        "message": f"  {u} ({t})"
-                    })
-                exit(1)
-            else:
-                ioc_common.logit({
-                    "level"  : "EXCEPTION",
-                    "message": f"{jail} not found!"
-                })
-
             if not force:
                 ioc_common.logit({
                     "level"  : "WARNING",
-                    "message": f"\nThis will destroy jail {uuid} ({tag})"
+                    "message": f"\nThis will destroy jail {jail}"
                 })
 
                 if not click.confirm("\nAre you sure?"):
                     continue  # no, continue to next jail
 
-            status, _ = ioc.IOCage().list("jid", uuid=uuid)
-
-            # If the jail is not running, let's do this thing.
-            if status and not force:
-                ioc_common.logit({
-                    "level"  : "EXCEPTION",
-                    "message": f"{uuid} ({tag}) is running.\nPlease stop"
-                               " it first!"
-                })
-            elif status and force:
-                ioc_common.logit({
-                    "level"  : "INFO",
-                    "message": f"Stopping {uuid} ({tag})."
-                })
-
-            iocage.destroy(path)
+            ioc.IOCage(jail, skip_jails=True).destroy_jail()
     elif jails and release:
-        iocage = ioc.IOCage(skip_jails=True)
-
         for release in jails:
-            path = f"{pool}/iocage/releases/{release}"
-
             if not force:
                 ioc_common.logit({
                     "level"  : "WARNING",
@@ -152,12 +72,7 @@ def cli(force, release, download, jails):
                 if not click.confirm("\nAre you sure?"):
                     continue
 
-            iocage.destroy(path, parse=True)
-
-            if download:
-                path = f"{pool}/iocage/download/{release}"
-                iocage.destroy(path, parse=True)
-
+            ioc.IOCage(release, skip_jails=True).destroy_release(download)
     elif not jails and release:
         ioc_common.logit({
             "level"  : "EXCEPTION",
