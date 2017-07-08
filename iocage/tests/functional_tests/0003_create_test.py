3274797dc81bc20e17fd424f09fed5e60946e41a
Brandon Schneider <brandon@ixsystems.com>
2017-07-07 19:57:05 -0500

    Fix `create` test

--- iocage/tests/functional_tests/0003_create_test.py
+++ iocage/tests/functional_tests/0003_create_test.py
@@ -35,19 +35,16 @@ require_zpool = pytest.mark.require_zpool
 @require_root
 @require_zpool
 def test_create(release, hardened):
-    prop = "tag=test"
-    prop_short = "tag=test_short"
-
     if hardened:
         release = release.replace("-RELEASE", "-STABLE")
         release = re.sub(r"\W\w.", "-", release)
 
     runner = CliRunner()
     result = runner.invoke(ioc.cli,
-                           ["create", "-r", release, prop, "-u",
+                           ["create", "-r", release, "-u",
                             "771ec0cf-afdd-455d-9245-4a890e228325"])
     result_short = runner.invoke(ioc.cli, ["create", "-r", release, "-s",
-                                           prop_short, "-u", "dfb013e5"])
+                                           "-u", "dfb013e5"])
 
     assert result.exit_code == 0
     assert result_short.exit_code == 0
