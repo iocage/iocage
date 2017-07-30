322fa2ca3c0ea41cb1df38b0375c83ad9a019e96
Stefan Grönke <stefan@gronke.net>
2017-07-30 19:00:29 +0200

    fix lib wrapper *args

--- iocage/__init__.py
+++ iocage/__init__.py
@@ -9,32 +9,44 @@ __dirname = os.path.dirname(__file__)
 iocage_lib_dir = os.path.join(__dirname, "lib")
 sys.path = [iocage_lib_dir] + sys.path
 
+
 class Releases:
- def __new__(self, **kwargs):
-   import Releases
-   return Releases.Releases(**kwargs)
+
+   def __new__(*args, **kwargs):
+      import Releases
+      return Releases.Releases(*args[1:], **kwargs)
+
 
 class Release:
- def __new__(self, **kwargs):
-   import Releases
-   return Releases.Releases(**kwargs)
+
+   def __new__(*args, **kwargs):
+      import Releases
+      return Releases.Releases(*args[1:], **kwargs)
+
 
 class Jails:
- def __new__(self, **kwargs):
-   import Jails
-   return Jails.Jails(**kwargs)
+
+   def __new__(*args, **kwargs):
+      import Jails
+      return Jails.Jails(*args[1:], **kwargs)
+
 
 class Jail:
- def __new__(self, **kwargs):
-   import Jail
-   return Jail.Jail(**kwargs)
+   def __new__(*args, **kwargs):
+
+      import Jail
+      return Jail.Jail(*args[1:], **kwargs)
+
 
 class Host:
- def __new__(self, **kwargs):
-   import Host
-   return Host.Host(**kwargs)
+
+   def __new__(*args, **kwargs):
+      import Host
+      return Host.Host(*args[1:], **kwargs)
+
 
 class Logger:
- def __new__(self, **kwargs):
-   import Logger
-   return Logger.Logger(**kwargs)
+
+   def __new__(*args, **kwargs):
+      import Logger
+      return Logger.Logger(*args[1:], **kwargs)
