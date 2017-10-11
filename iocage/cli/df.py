f925194f4852c1d68ab0a8ddb41b98413d47ca8d
Brandon Schneider <github@bschneider.email>
2017-10-11 03:14:25 -0500

    Remove unused argument for `df`
    This was removed from the class some time ago, forgot to remove it from this call ;)
    Closes #383

--- iocage/cli/df.py
+++ iocage/cli/df.py
@@ -24,39 +24,48 @@
 """df module for the cli."""
 
 import click
-import texttable
-
 import iocage.lib.ioc_common as ioc_common
 import iocage.lib.iocage as ioc
+import texttable
 
 
 @click.command(name="df", help="Show resource usage of all jails.")
-@click.option("--header", "-h", "-H", is_flag=True, default=True,
-              help="For scripting, use tabs for separators.")
-@click.option("--long", "-l", "_long", is_flag=True, default=False,
-              help="Show the full uuid.")
-@click.option("--sort", "-s", "_sort", default="name", nargs=1,
-              help="Sorts the list by the given type")
+@click.option(
+    "--header",
+    "-h",
+    "-H",
+    is_flag=True,
+    default=True,
+    help="For scripting, use tabs for separators.")
+@click.option(
+    "--long",
+    "-l",
+    "_long",
+    is_flag=True,
+    default=False,
+    help="Show the full uuid.")
+@click.option(
+    "--sort",
+    "-s",
+    "_sort",
+    default="name",
+    nargs=1,
+    help="Sorts the list by the given type")
 def cli(header, _long, _sort):
     """Allows a user to show resource usage of all jails."""
     table = texttable.Texttable(max_width=0)
-    jail_list = ioc.IOCage(exit_on_error=True).df(long=_long)
+    jail_list = ioc.IOCage(exit_on_error=True).df()
 
     sort = ioc_common.ioc_sort("df", _sort, exit_on_error=True)
     jail_list.sort(key=sort)
+
     if header:
         jail_list.insert(0, ["NAME", "CRT", "RES", "QTA", "USE", "AVA"])
         # We get an infinite float otherwise.
         table.set_cols_dtype(["t", "t", "t", "t", "t", "t"])
         table.add_rows(jail_list)
 
-        ioc_common.logit({
-            "level"  : "INFO",
-            "message": table.draw()
-        })
+        ioc_common.logit({"level": "INFO", "message": table.draw()})
     else:
         for jail in jail_list:
-            ioc_common.logit({
-                "level"  : "INFO",
-                "message": "\t".join(jail)
-            })
+            ioc_common.logit({"level": "INFO", "message": "\t".join(jail)})
