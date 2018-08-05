6e21d1be3a28ffcbb5dd855595660046647dde73
Stefan Grönke <stefan@gronke.net>
2018-08-05 17:18:30 +0000

    move manpage files to top directory

--- setup.py
+++ setup.py
@@ -30,10 +30,10 @@ from setuptools import find_packages, setup
 
 if os.path.isdir("/".join([sys.prefix, "etc/init.d"])):
     _data = [('etc/init.d', ['rc.d/iocage']),
-             ('man/man8', ['iocage/iocage.8.gz'])]
+             ('man/man8', ['iocage.8.gz'])]
 else:
     _data = [('etc/rc.d', ['rc.d/iocage']),
-             ('man/man8', ['iocage/iocage.8.gz'])]
+             ('man/man8', ['iocage.8.gz'])]
 
 if os.path.isdir("/".join([sys.prefix, "share/zsh/site-functions/"])):
     _data.append(('share/zsh/site-functions', ['zsh-completion/_iocage']))
