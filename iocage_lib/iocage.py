2fde7aeef7008f482d233a4ea1cf91948337ebb1
Brandon Schneider <github@bschneider.email>
2018-09-27 15:29:45 -0500

    exec_clean now affects exec as expected.
    Upstream required -l to be passed to jexec to clean the environment. Otherwise it was just for the jail initialization.
    
    Closes: #610
    FreeNAS Ticket: #48581

--- iocage_lib/iocage.py
+++ iocage_lib/iocage.py
@@ -769,6 +769,12 @@ class IOCage(object):
                 silent=self.silent)
 
         uuid, path = self.__check_jail_existence__()
+        exec_clean = self.get('exec_clean')
+
+        if exec_clean == '1':
+            su_env = None
+        else:
+            su_env = os.environ.copy()
 
         if pkg:
             ip4_addr = self.get("ip4_addr")
@@ -804,7 +810,8 @@ class IOCage(object):
             console=console,
             silent=self.silent,
             msg_return=msg_return,
-            pkg=pkg).exec_jail()
+            pkg=pkg,
+            su_env=su_env).exec_jail()
 
         if not console:
             if err:
