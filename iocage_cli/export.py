6d0753dc94c5fb441c0b0c6796f72f938e199c94
Stefan Grönke <stefan@gronke.net>
2018-10-05 19:00:39 +0000

    copy import and export commands from libiocage/ioc

--- iocage_cli/export.py
+++ iocage_cli/export.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2014-2018, iocage
+# Copyright (c) 2017-2018, Stefan Grönke
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -21,16 +22,75 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-"""export module for the cli."""
+"""Export a jail from the CLI."""
 import click
+import os.path
 
-import iocage_lib.iocage as ioc
+import iocage.errors
+import iocage.Filter
+import iocage.Jail
+import iocage.Jails
+import iocage.Logger
+import iocage.Releases
+import iocage.Resource
+
+from .shared.click import IocageClickContext
 
 __rootcmd__ = True
 
 
-@click.command(name="export", help="Exports a specified jail.")
+@click.command(name="export", help="Export a jail to a backup archive")
+@click.pass_context
 @click.argument("jail", required=True)
-def cli(jail):
-    """Make a recursive snapshot of the jail and export to a file."""
-    ioc.IOCage(jail=jail).export()
+@click.argument("destination", required=True)
+@click.option(
+    "-s", "--standalone",
+    default=False,
+    is_flag=True,
+    help="Exports the jails root dataset independently"
+)
+# @click.option(
+#     "-r", "--recursive",
+#     default=False,
+#     is_flag=True,
+#     help="Include ZFS snapshots. Implies --standalone"
+# )
+def cli(
+    ctx: IocageClickContext,
+    jail: str,
+    destination: str,
+    standalone: bool
+) -> None:
+    """
+    Backup a jail.
+
+    The selected jail will be exported to a gzip compressed tar archive stored
+    as the destination path.
+    """
+    logger = ctx.parent.logger
+    zfs: iocage.ZFS.ZFS = ctx.parent.zfs
+    host: iocage.Host.HostGenerator = ctx.parent.host
+    print_events = ctx.parent.print_events
+
+    # Recursive exports cannot be imported at the current time
+    recursive = False
+
+    ioc_jail = iocage.Jail.JailGenerator(
+        jail,
+        logger=logger,
+        zfs=zfs,
+        host=host
+    )
+
+    if os.path.isfile(destination) is True:
+        logger.error(f"The destination {destination} already exists")
+        exit(1)
+
+    try:
+        print_events(ioc_jail.backup.export(
+            destination,
+            standalone=standalone,
+            recursive=recursive
+        ))
+    except iocage.errors.IocageException:
+        exit(1)
--- iocage_cli/import.py
+++ iocage_cli/import.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2014-2018, iocage
+# Copyright (c) 2017-2018, Stefan Grönke
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -21,16 +22,47 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-"""import module for the cli."""
+"""Export a jail from the CLI."""
 import click
 
-import iocage_lib.iocage as ioc
+import iocage.errors
+import iocage.Jail
+import iocage.Host
+import iocage.ZFS
+
+from .shared.click import IocageClickContext
 
 __rootcmd__ = True
 
 
-@click.command(name="import", help="Import a specified jail.")
+@click.command(name="import", help="Import a jail from a backup archive")
+@click.pass_context
 @click.argument("jail", required=True)
-def cli(jail):
-    """Import from an iocage export."""
-    ioc.IOCage(jail=jail).import_()
+@click.argument("source", required=True)
+def cli(
+    ctx: IocageClickContext,
+    jail: str,
+    source: str
+) -> None:
+    """Restore a jail from a backup archive."""
+    logger = ctx.parent.logger
+    zfs: iocage.ZFS.ZFS = ctx.parent.zfs
+    host: iocage.Host.HostGenerator = ctx.parent.host
+    print_events = ctx.parent.print_events
+
+    ioc_jail = iocage.Jail.JailGenerator(
+        dict(name=jail),
+        logger=logger,
+        zfs=zfs,
+        host=host,
+        new=True
+    )
+
+    if ioc_jail.exists is True:
+        logger.error(f"The jail {jail} already exists")
+        exit(1)
+
+    try:
+        print_events(ioc_jail.backup.restore(source))
+    except iocage.errors.IocageException:
+        exit(1)
