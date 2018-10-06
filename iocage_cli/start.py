080b12b4402ed5a1a558c6f43bc0d5dd6695c470
Stefan Grönke <stefan@gronke.net>
2018-10-06 20:35:55 +0000

    copy start command from libiocage/ioc

--- iocage_cli/start.py
+++ iocage_cli/start.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2014-2018, iocage
+# Copyright (c) 2017-2018, Stefan Grönke
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -21,34 +22,160 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-"""start module for the cli."""
+"""Start jails from the CLI.."""
+import typing
 import click
 
-import iocage_lib.ioc_common as ioc_common
-import iocage_lib.iocage as ioc
+import iocage.errors
+import iocage.Jails
+import iocage.Logger
+
+from .shared.click import IocageClickContext
+from .shared.jail import set_properties
 
 __rootcmd__ = True
 
 
 @click.command(name="start", help="Starts the specified jails or ALL.")
-@click.option("--rc", default=False, is_flag=True,
-              help="Will start all jails with boot=on, in the specified"
-                   " order with smaller value for priority starting first.")
+@click.pass_context
+@click.option(
+    "--rc",
+    default=False,
+    is_flag=True,
+    help=(
+        "Will start all jails with boot=on, in the specified "
+        "order with smaller value for priority starting first."
+    )
+)
+@click.option(
+    "--option", "-o",
+    "temporary_config_override",
+    multiple=True,
+    help="Temporarily override jail config options"
+)
 @click.argument("jails", nargs=-1)
-def cli(rc, jails):
-    """
-    Looks for the jail supplied and passes the uuid, path and configuration
-    location to start_jail.
-    """
-    if not jails and not rc:
-        ioc_common.logit({
-            "level"  : "EXCEPTION",
-            "message": 'Usage: iocage start [OPTIONS] JAILS...\n'
-                       '\nError: Missing argument "jails".'
-        })
+def cli(
+    ctx: IocageClickContext,
+    rc: bool,
+    temporary_config_override: typing.Tuple[str, ...],
+    jails: typing.Tuple[str, ...]
+) -> None:
+    """Start one or many jails."""
+    logger = ctx.parent.logger
+    start_args = {
+        "zfs": ctx.parent.zfs,
+        "host": ctx.parent.host,
+        "logger": logger,
+        "print_function": ctx.parent.print_events
+    }
 
-    if rc:
-        ioc.IOCage(rc=rc, silent=True).start()
+    if (rc is False) and (len(jails) == 0):
+        logger.error("No jail selector provided")
+        exit(1)
+
+    elif rc is True:
+        if len(jails) > 0:
+            logger.error("Cannot use --rc and jail selectors simultaniously")
+            exit(1)
+        _autostart(**start_args)
     else:
-        for jail in jails:
-            ioc.IOCage(jail=jail, rc=rc).start()
+        start_normal_successful = _normal(
+            jails,
+            temporary_config_override=temporary_config_override,
+            **start_args
+        )
+        if start_normal_successful is False:
+            exit(1)
+
+
+def _autostart(
+    zfs: iocage.ZFS.ZFS,
+    host: iocage.Host.HostGenerator,
+    logger: iocage.Logger.Logger,
+    print_function: typing.Callable[
+        [typing.Generator[iocage.events.IocageEvent, None, None]],
+        None
+    ]
+) -> None:
+
+    filters = ("boot=yes", "running=no", "template=no,-",)
+
+    ioc_jails = iocage.Jails.Jails(
+        zfs=zfs,
+        host=host,
+        logger=logger,
+        filters=filters
+    )
+
+    # sort jails by their priority
+    jails = sorted(
+        list(ioc_jails),
+        key=lambda x: x.config["priority"]
+    )
+
+    failed_jails = []
+    for jail in jails:
+        try:
+            jail.start()
+        except iocage.errors.IocageException:
+            failed_jails.append(jail)
+            continue
+
+        logger.log(f"{jail.humanreadable_name} running as JID {jail.jid}")
+
+    if len(failed_jails) > 0:
+        exit(1)
+
+    exit(0)
+
+
+def _normal(
+    filters: typing.Tuple[str, ...],
+    temporary_config_override: typing.Tuple[str, ...],
+    zfs: iocage.ZFS.ZFS,
+    host: iocage.Host.HostGenerator,
+    logger: iocage.Logger.Logger,
+    print_function: typing.Callable[
+        [typing.Generator[iocage.events.IocageEvent, None, None]],
+        None
+    ]
+) -> bool:
+
+    filters += ("template=no,-",)
+
+    jails = iocage.Jails.JailsGenerator(
+        logger=logger,
+        zfs=zfs,
+        host=host,
+        filters=filters
+    )
+
+    changed_jails = []
+    failed_jails = []
+    for jail in jails:
+        try:
+            set_properties(
+                properties=temporary_config_override,
+                target=jail
+            )
+        except iocage.errors.IocageException:
+            exit(1)
+        try:
+            jail.require_jail_not_template()
+            print_function(jail.start())
+        except iocage.errors.IocageException:
+            failed_jails.append(jail)
+            continue
+
+        logger.log(f"{jail.humanreadable_name} running as JID {jail.jid}")
+        changed_jails.append(jail)
+
+    if len(failed_jails) > 0:
+        return False
+
+    if len(changed_jails) == 0:
+        jails_input = " ".join(list(jails))
+        logger.error(f"No jails started your input: {jails_input}")
+        return False
+
+    return True
