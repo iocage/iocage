49114e7c5a6cd6fb7584ae7c76dc7248620ac65f
Stefan Grönke <stefan@gronke.org>
2018-10-08 20:52:53 +0200

    copy fstab command from libiocage/ioc (#648)

--- iocage_cli/fstab.py
+++ iocage_cli/fstab.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2014-2018, iocage
+# Copyright (c) 2017-2018, Stefan Grönke
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -21,119 +22,219 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-"""fstab module for the cli."""
+"""View and manipulate fstab files of a jail."""
+import typing
+import errno
 import click
-import iocage_lib.ioc_common as ioc_common
-import iocage_lib.iocage as ioc
+import os
+
+import iocage.errors
+import iocage.Logger
+import iocage.Host
+import iocage.helpers
+import iocage.Jail
+import iocage.Config.Jail.File.Fstab
+
+from .shared.jail import get_jail
+from .shared.click import IocageClickContext
 
 __rootcmd__ = True
+FstabLine = iocage.Config.Jail.File.Fstab.FstabLine
 
 
-@click.command(name="fstab", help="Manipulate the specified jails fstab.")
-@click.argument("jail")
-@click.argument("fstab_string", nargs=-1)
-@click.option("--add", "-a", "action",
-              help="Adds an entry to the jails fstab and mounts it.",
-              flag_value="add")
-@click.option("--remove", "-r", "action",
-              help="Removes an entry from the jails fstab and unmounts it.",
-              flag_value="remove")
-@click.option("--edit", "-e", "action",
-              help="Opens up the fstab file in your environments EDITOR.",
-              flag_value="edit")
-@click.option("--replace", "-R",
-              help="Replace an entry by index number", nargs=1)
-@click.option("--list", "-l", "action",
-              help="Lists the jails fstab.", flag_value="list")
-@click.option("--header", "-h", "-H", is_flag=True, default=True,
-              help="For scripting, use tabs for separators.")
-def cli(action, fstab_string, jail, header, replace):
-    """
-    Looks for the jail supplied and passes the uuid, path and configuration
-    location to manipulate the fstab.
-    """
-    index = None if not replace else replace
-    _index = False
-    add_path = False
-    fstab_string = list(fstab_string)
-    action = action if not replace else "replace"
-
-    if not fstab_string and action != "edit" and action != "list":
-        ioc_common.logit({
-            "level": "EXCEPTION",
-            "message": "Please supply a fstab entry or jail!"
-        })
-
-    # The user will expect to supply a string, the API would prefer these
-    # separate. If the user supplies a quoted string, we will split it,
-    # otherwise the format is acceptable to be imported directly.
-
-    if len(fstab_string) == 1:
-        try:
-            source, destination, fstype, options, dump, _pass = fstab_string[
-                0].split()
-        except ValueError:
-            # We're going to assume this is an index number.
-            try:
-                index = int(fstab_string[0])
-
-                _index = True
-                source, destination, fstype, options, dump, _pass = "", "", \
-                                                                    "", "", \
-                                                                    "", ""
-            except TypeError:
-                ioc_common.logit({
-                    "level": "EXCEPTION",
-                    "message": "Please specify either a valid fstab "
-                               "entry or an index number."
-                })
-            except ValueError:
-                # We will assume this is just a source, and will do a readonly
-                # nullfs mount
-                source = fstab_string[0]
-                destination = source
-                fstype = "nullfs"
-                options = "ro"
-                dump = "0"
-                _pass = "0"
-    elif action == "list":
-        # We don't need these
-        source, destination, fstype, options, dump, _pass = "", "", \
-                                                            "", "", \
-                                                            "", ""
+def _get_relpath(path: str, jail: iocage.Jail.JailGenerator) -> str:
+    if path.startswith(jail.root_path) is True:
+        return path[len(jail.root_path.rstrip("/")):]
     else:
-        if action != "edit":
-            try:
-                source, destination, fstype, options, dump, _pass = \
-                    fstab_string
-            except ValueError:
-                ioc_common.logit({
-                    "level": "EXCEPTION",
-                    "message": "Please specify a valid fstab entry!\n\n"
-                               "Example:\n  /the/source /dest FSTYPE "
-                               "FSOPTIONS FSDUMP FSPASS"
-                })
-        else:
-            source, destination, fstype, options, dump, _pass = "", "", \
-                                                                "", "", \
-                                                                "", ""
+        return path
 
-    if not _index:
-        add_path = True
 
-    fstab = ioc.IOCage(jail=jail).fstab(
-        action, source, destination, fstype, options, dump, _pass, index=index,
-        add_path=add_path, header=header)
+def _get_abspath(path: str, jail: iocage.Jail.JailGenerator) -> str:
+    result = str(os.path.realpath(os.path.join(
+        jail.root_path,
+        _get_relpath(path, jail).lstrip("/")
+    )))
 
-    if action == "list":
-        if header:
-            ioc_common.logit({
-                "level": "INFO",
-                "message": fstab
-            })
-        else:
-            for f in fstab:
-                ioc_common.logit({
-                    "level": "INFO",
-                    "message": f"{f[0]}\t{f[1]}"
-                })
+    if result.startswith(jail.root_path):
+        return result
+
+    raise iocage.errors.InsecureJailPath(
+        path=result,
+        logger=jail.logger
+    )
+
+
+@click.command(
+    name="add"
+)
+@click.pass_context
+@click.argument(
+    "source",
+    nargs=1,
+    required=True
+)
+@click.argument(
+    "destination",
+    nargs=-1,
+    required=False
+)
+@click.argument("jail", nargs=1, required=True)
+@click.option("--read-write", "-rw", is_flag=True, default=False)
+def cli_add(
+    ctx: IocageClickContext,
+    source: str,
+    destination: typing.Tuple[str],
+    jail: str,
+    read_write: bool
+) -> None:
+    """Add lines to a jails fstab file."""
+    ioc_jail = get_jail(jail, ctx.parent)
+
+    if len(destination) == 0:
+        desination_path = source
+    else:
+        desination_path = destination[0]
+
+    if os.path.exists(source) is False:
+        ctx.parent.logger.error(
+            f"The mount source {source} is does not exist"
+        )
+        exit(1)
+
+    if os.path.isdir(source) is False:
+        ctx.parent.logger.error(
+            f"The mount source {source} is not a directory"
+        )
+        exit(1)
+
+    try:
+        mount_opts = "rw" if read_write is True else "ro"
+        desination_path = _get_abspath(desination_path, ioc_jail)
+
+        fstab = ioc_jail.fstab
+        fstab.read_file()
+        fstab.new_line(
+            source=source,
+            destination=desination_path,
+            type="nullfs",
+            options=mount_opts,
+            dump="0",
+            passnum="0",
+            comment=None
+        )
+
+        # ensure destination directory exists
+        try:
+            os.makedirs(desination_path)
+        except OSError as exc:  # Python >2.5
+            if exc.errno == errno.EEXIST and os.path.isdir(desination_path):
+                pass
+            else:
+                raise
+
+        fstab.save()
+        ctx.parent.logger.log(
+            f"fstab mount added: {source} -> {desination_path} ({mount_opts})"
+        )
+        exit(0)
+    except iocage.errors.IocageException:
+        exit(1)
+
+
+@click.command(
+    name="show"
+)
+@click.pass_context
+@click.argument("jail", nargs=1, required=True)
+def cli_show(ctx: IocageClickContext, jail: str) -> None:
+    """Show a jails fstab file."""
+    ioc_jail = get_jail(jail, ctx.parent)
+    if os.path.isfile(ioc_jail.fstab.path):
+        with open(ioc_jail.fstab.path, "r") as f:
+            print(f.read())
+
+
+@click.command(
+    name="rm"
+)
+@click.argument(
+    "source",
+    nargs=1,
+    required=False
+)
+@click.argument("jail", nargs=1, required=True)
+@click.pass_context
+def cli_rm(ctx: IocageClickContext, source: str, jail: str) -> None:
+    """Remove a line from a jails fstab file."""
+    ioc_jail = get_jail(jail, ctx.parent)
+    fstab = ioc_jail.fstab
+    destination = None
+    i = 0
+
+    try:
+        fstab.read_file()
+        for existing_line in fstab:
+            i += 1
+            if isinstance(existing_line, FstabLine) is False:
+                continue
+            if existing_line["source"] == source:
+                destination = fstab[i - 1]["destination"]
+                del fstab[i - 1]
+                fstab.save()
+                break
+    except iocage.errors.IocageException:
+        exit(1)
+
+    if destination is None:
+        ctx.parent.logger.error("no matching fstab line found")
+        exit(1)
+
+    ctx.parent.logger.log(f"fstab mount removed: {source} -> {destination}")
+
+
+class FstabCli(click.MultiCommand):
+    """Python Click fstab subcommand boilerplate."""
+
+    def list_commands(self, ctx: click.core.Context) -> list:
+        """Mock subcommands for Python Click."""
+        return [
+            "show",
+            "add",
+            "rm"
+        ]
+
+    def get_command(
+        self,
+        ctx: click.core.Context,
+        cmd_name: str
+    ) -> click.core.Command:
+        """Wrap subcommand for Python Click."""
+        command: typing.Optional[click.core.Command] = None
+
+        if cmd_name == "show":
+            command = cli_show
+        elif cmd_name == "add":
+            command = cli_add
+        elif cmd_name == "rm":
+            command = cli_rm
+
+        if command is None:
+            raise NotImplementedError("action does not exist")
+
+        return command
+
+
+@click.group(
+    name="fstab",
+    cls=FstabCli,
+    context_settings=dict(
+        ignore_unknown_options=True,
+    )
+)
+@click.pass_context
+def cli(
+    ctx: IocageClickContext
+) -> None:
+    """View and manipulate a jails fstab file."""
+    ctx.logger = ctx.parent.logger
+    ctx.host = ctx.parent.host
