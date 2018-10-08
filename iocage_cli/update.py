ddaf6ca1430c272f863df1fca3aedce57cd28419
Stefan Grönke <stefan@gronke.net>
2018-10-08 07:05:12 +0200

    copy update command from libiocage/ioc

--- iocage_cli/update.py
+++ iocage_cli/update.py
@@ -1,4 +1,5 @@
 # Copyright (c) 2014-2018, iocage
+# Copyright (c) 2017-2018, Stefan Grönke
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -21,20 +22,68 @@
 # STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
 # IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-"""update module for the cli."""
+"""
+Apply operating system updates to jails.
+
+When a release was fetched, patches are downloaded. Standalone jails that
+were forked from a release that received new updates needs to apply them.
+No network connection is required, as the previously downloaded patches
+are temporarily mounted and applied with securelevel=0.
+"""
 import click
-import iocage_lib.iocage as ioc
+import typing
+
+import iocage.errors
+import iocage.Jails
+import iocage.Logger
+import iocage.Config.Jail.File.Fstab
+
+from .shared.click import IocageClickContext
 
 __rootcmd__ = True
 
 
-@click.command(
-    name="update",
-    help="Run freebsd-update to update a specified "
-    "jail to the latest patch level.")
-@click.argument("jail", required=True)
-def cli(jail):
-    """Update the supplied jail to the latest patchset"""
-    iocage = ioc.IOCage(jail=jail)
+@click.command(name="start", help="Starts the specified jails or ALL.")
+@click.pass_context
+@click.argument("jails", nargs=-1)
+def cli(
+    ctx: IocageClickContext,
+    jails: typing.Tuple[str, ...]
+) -> typing.Optional[bool]:
+    """Update jails with patches from their releases."""
+    logger = ctx.parent.logger
+    print_function = ctx.parent.print_events
 
-    iocage.update()
+    if len(jails) == 0:
+        logger.error("No jail selector provided")
+        exit(1)
+
+    filters = jails + ("template=no,-", "basejail=no",)
+    ioc_jails = iocage.Jails.JailsGenerator(
+        logger=logger,
+        host=ctx.parent.host,
+        zfs=ctx.parent.zfs,
+        filters=filters
+    )
+
+    changed_jails = []
+    failed_jails = []
+    for jail in ioc_jails:
+        try:
+            changed = print_function(jail.updater.apply())
+            if changed is True:
+                changed_jails.append(jail)
+        except iocage.errors.UpdateFailure:
+            failed_jails.append(jail)
+
+    if len(failed_jails) > 0:
+        return False
+
+    if len(changed_jails) == 0:
+        jails_input = " ".join(list(jails))
+        logger.error(
+            f"No non-basejail was updated or matched your input: {jails_input}"
+        )
+        return False
+
+    return True
