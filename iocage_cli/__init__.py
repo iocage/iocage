ba88d2bf883599c91731b7f578cd2462f883b833
Brandon Schneider <brandon@ixsystems.com>
2018-11-06 19:33:18 -0600

    Bump to 2.0 for this branch

--- iocage_cli/__init__.py
+++ iocage_cli/__init__.py
@@ -83,7 +83,7 @@ def print_version(ctx, param, value):
 
     if not value or ctx.resilient_parsing:
         return
-    print("Version\t1.0 ALPHA 1")
+    print("Version\t2.0 ALPHA 2018/11")
     sys.exit()
 
 
