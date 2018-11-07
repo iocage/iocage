fbb427affbc82b0a62f02881e22c69f698113b41
Brandon Schneider <github@bschneider.email>
2018-11-06 19:34:40 -0600

    Bump to RC

--- iocage_cli/__init__.py
+++ iocage_cli/__init__.py
@@ -63,7 +63,7 @@ def print_version(ctx, param, value):
 
     if not value or ctx.resilient_parsing:
         return
-    print("Version\t1.0 ALPHA 1")
+    print("Version\t1.0 RC 2018/11")
     sys.exit()
 
 
