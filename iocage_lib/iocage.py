3cd179ca8b113e948483288e0cabf9ae8aed4efe
Brandon Schneider <github@bschneider.email>
2018-11-13 21:42:54 -0600

    Add missing return for setting default prop
    This avoids a harmless jail not found error that was erroneously throwing on default configuration setting.

--- iocage_lib/iocage.py
+++ iocage_lib/iocage.py
@@ -1460,6 +1460,7 @@ class IOCage(object):
 
         if default:
             ioc_json.IOCJson(self.iocroot).json_set_value(prop, default=True)
+            return
 
         uuid, path = self.__check_jail_existence__()
         iocjson = ioc_json.IOCJson(
