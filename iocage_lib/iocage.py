dd2ad868ca72e40e1902007ecf46bfffe8b326c6
bugclerk <40872210+bugclerk@users.noreply.github.com>
2018-12-06 20:34:22 +0300

    Use en_US.UTF-8 or users LANG by default for console/exec (#106)
    Ticket: #59193

--- iocage_lib/iocage.py
+++ iocage_lib/iocage.py
@@ -772,11 +772,14 @@ class IOCage(object):
         if exec_clean == '1':
             env_path = '/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:'\
                    '/usr/local/bin:/root/bin'
+            env_lang = os.environ.get('LANG', 'en_US.UTF-8')
             su_env = {
                 'PATH': env_path,
                 'PWD': '/',
                 'HOME': '/',
-                'TERM': 'xterm-256color'
+                'TERM': 'xterm-256color',
+                'LANG': env_lang,
+                'LC_ALL': env_lang
                 }
         else:
             su_env = os.environ.copy()
