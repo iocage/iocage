34b88710b2b002e2796b665d69fc3d8edde2c637
Brandon Schneider <github@bschneider.email>
2018-12-10 12:18:54 -0600

    Nicer!
    @william-gr keeps reminding me how ugly my shell^W python is.

--- iocage_lib/ioc_json.py
+++ iocage_lib/ioc_json.py
@@ -374,7 +374,7 @@ class IOCConfiguration(IOCZFS):
         else:
             # Catch all users migrating from old prop value of none, which
             # meant auto
-            if (current_conf_version == '13' or current_conf_version == '12') \
+            if current_conf_version in ('12', '13') \
                     and conf['vnet_default_interface'] == 'none':
                 conf['vnet_default_interface'] = 'auto'
 
