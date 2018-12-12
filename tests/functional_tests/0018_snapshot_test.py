b3a087d4d389fc5aecfb1f69ba3c2da7c041ae88
Waqar Ahmed <waqarsonic1@gmail.com>
2018-12-13 01:37:35 +0500

    Fstab/Snapshot Tests
    This commit adds tests relating to fstab and snapshot related commands

---
+++ tests/functional_tests/0018_snapshot_test.py
@@ -0,0 +1,57 @@
+# Copyright (c) 2014-2018, iocage
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted providing that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import pytest
+
+
+require_root = pytest.mark.require_root
+require_zpool = pytest.mark.require_zpool
+
+
+SNAP_NAME = 'snaptest'
+
+
+def common_function(invoke_cli, jails, skip_test):
+    skip_test(not jails)
+
+    jail = jails[0]
+    invoke_cli(
+        ['snapshot', jail.name, '-n', SNAP_NAME]
+    )
+
+    # We use count because of template jails
+    for snap in jail.recursive_snapshots:
+        assert SNAP_NAME == snap.id.split('@')[1]
+
+
+@require_root
+@require_zpool
+def test_01_snapshot_of_jail(invoke_cli, resource_selector, skip_test):
+    common_function(invoke_cli, resource_selector.jails, skip_test)
+
+
+@require_root
+@require_zpool
+def test_02_snapshot_of_template_jail(invoke_cli, resource_selector, skip_test):
+    common_function(invoke_cli, resource_selector.template_jails, skip_test)
