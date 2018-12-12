ebde26a3ea0e410f8a70f495216feae353d89962
Waqar Ahmed <waqarsonic1@gmail.com>
2018-12-13 01:36:37 +0500

    Console/Rename/Restart Tests
    This commit adds tests relating to console, rename and restart commands

---
+++ tests/functional_tests/0015_rename_test.py
@@ -0,0 +1,59 @@
+# Copyright (c) 2014-2018, iocage
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted providing that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import pytest
+
+
+require_root = pytest.mark.require_root
+require_zpool = pytest.mark.require_zpool
+
+
+def _common(invoke_cli, orig_jail, jail):
+    renamed = orig_jail.name + '_renamed'
+    invoke_cli(
+        ['rename', orig_jail.name, renamed]
+    )
+
+    assert orig_jail.exists is False
+    assert jail(renamed).exists is True
+
+
+@require_root
+@require_zpool
+def test_01_rename_jail(invoke_cli, resource_selector, skip_test, jail):
+    jails = resource_selector.jails
+    skip_test(not jails)
+
+    orig_jail = jails[0]
+    _common(invoke_cli, orig_jail, jail)
+
+
+@require_root
+@require_zpool
+def test_02_rename_template_jail(invoke_cli, resource_selector, skip_test, jail):
+    jails = resource_selector.template_jails
+    skip_test(not jails)
+
+    orig_jail = jails[0]
+    _common(invoke_cli, orig_jail, jail)
