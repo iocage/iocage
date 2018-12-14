46be67a20a376af2f658d74ed4e7cdc3f3f31b94
bugclerk <40872210+bugclerk@users.noreply.github.com>
2018-12-14 20:11:27 +0100

    tkt-64323: Only capitalize fetch RELEASE if everything is default (by skarekrow) (#152)
    * Only capitalize fetch RELEASE if everything is default
    
    Not a local file directory or custom mirror.
    
    Closes: #626
    FreeNAS Ticket: #64143
    (cherry picked from commit 934567ec8f4948e721b5d1387bfed6a4f85819c5)
    
    * Address feedback
    
    Remove the upper, users will need to supply a proper name now. Also make checking if the release is newer best effort, as if the users choice of naming causes issues, it's hard to be sure.

--- iocage_cli/create.py
+++ iocage_cli/create.py
@@ -106,6 +106,22 @@ def cli(release, template, count, props, pkglist, basejail, thickjail, empty,
     elif release and release.lower() == "latest":
         release = ioc_common.parse_latest_release()
 
+    if release:
+        try:
+            ioc_common.check_release_newer(release)
+        except ValueError:
+            # We're assuming they understand the implications of a custom
+            # scheme
+            iocroot = ioc.PoolAndDataset().get_iocroot()
+            path = f'{iocroot}/releases/{release}/root'
+            _release = ioc_common.get_jail_freebsd_version(path, release)
+
+            try:
+                ioc_common.check_release_newer(_release)
+            except ValueError:
+                # We tried
+                pass
+
     # We don't really care it's not a RELEASE at this point.
     release = template if template else release
 
