iocage/iocage

commit c15be3d74c2e9618fa5708d7a2924a096fa11402

Flatten Python module names (#592)
* rename iocage.lib to iocage_lib

* rename iocage/cli to iocage_cli

* update setup.py to iocage_lib and iocage_cli

* move manpage files to top directory

* move icage/tests/ to top level directory

* add missing dev dependency mock

* gitignore .pytest_cache/

* remove deprecated iocage/ directory

* ./iocage script for local cli testing/development

iocage_cli/create.py
@@ -1,0 +1,173 @@
+# Copyright (c) 2014-2017, iocage
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted providing that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+"""create module for the cli."""
+import json
+import os
+import re
+
+import click
+import iocage_lib.ioc_common as ioc_common
+import iocage_lib.iocage as ioc
+
+__rootcmd__ = True
+
+
+def validate_count(ctx, param, value):
+    """Takes a string, removes the commas and returns an int."""
+
+    if isinstance(value, str):
+        try:
+            value = value.replace(",", "")
+
+            return int(value)
+        except ValueError:
+            ioc_common.logit({
+                "level": "EXCEPTION",
+                "message": f"{value} is not a valid integer."
+            })
+    else:
+        return int(value)
+
+
+@click.command(name="create", help="Create a jail.")
+@click.option("--count", "-c", callback=validate_count, default="1",
+              help="Designate a number of jails to create. Jails are"
+                   " numbered sequentially.")
+@click.option("--release", "-r", required=False,
+              help="Specify the RELEASE to use for the new jail.")
+@click.option("--template", "-t", required=False,
+              help="Specify the template to use for the new jail instead of"
+                   " a RELEASE.")
+@click.option("--pkglist", "-p", default=None,
+              help="Specify a JSON file which manages the installation of"
+                   " each package in the newly created jail.")
+@click.option("--name", "-n", default=None,
+              help="Provide a specific name instead of an UUID for this jail.")
+@click.option("--uuid", "-u", "_uuid", default=None,
+              help="Provide a specific UUID for this jail.")
+@click.option("--basejail", "-b", is_flag=True, default=False,
+              help="Set the new jail type to a basejail. Basejails"
+                   " mount the specified RELEASE directories as nullfs"
+                   " mounts over the jail's directories.")
+@click.option("--thickjail", "-T", is_flag=True, default=False,
+              help="Set the new jail type to a thickjail. Thickjails"
+                   " are copied (not cloned) from specified RELEASE.")
+@click.option("--empty", "-e", is_flag=True, default=False,
+              help="Create an empty jail used for unsupported or custom"
+                   " jails.")
+@click.option("--short", "-s", is_flag=True, default=False,
+              help="Use a short UUID of 8 characters instead of the default"
+                   " 36.")
+@click.option("--force", "-f", is_flag=True, default=False,
+              help="Skip the interactive question.")
+@click.argument("props", nargs=-1)
+def cli(release, template, count, props, pkglist, basejail, thickjail, empty,
+        short, name, _uuid, force):
+
+    if name:
+        # noinspection Annotator
+        valid = True if re.match("^[a-zA-Z0-9\._-]+$", name) else False
+
+        if not valid:
+            ioc_common.logit({
+                "level": "EXCEPTION",
+                "message": f"Invalid character in {name}, please remove it."
+            })
+
+        # At this point we don't care
+        _uuid = name
+
+    if release and "=" in release:
+        ioc_common.logit({
+            "level": "EXCEPTION",
+            "message": "Please supply a valid RELEASE!"
+        })
+    elif release and release.lower() == "latest":
+        release = ioc_common.parse_latest_release()
+
+    if release:
+        release = release.upper()
+        ioc_common.check_release_newer(release)
+
+    # We don't really care it's not a RELEASE at this point.
+    release = template if template else release
+
+    if pkglist:
+        _pkgformat = """
+{
+    "pkgs": [
+    "foo",
+    "bar"
+    ]
+}"""
+
+        if not os.path.isfile(pkglist):
+            ioc_common.logit({
+                "level": "EXCEPTION",
+                "message": f"{pkglist} does not exist!\n"
+                           "Please supply a JSON file with the format:"
+                           f" {_pkgformat}"
+            })
+        else:
+            try:
+                # Just try to open the JSON with the right key.
+                with open(pkglist, "r") as p:
+                    json.load(p)["pkgs"]  # noqa
+            except json.JSONDecodeError:
+                ioc_common.logit({
+                    "level": "EXCEPTION",
+                    "message": "Please supply a valid"
+                               f" JSON file with the format:{_pkgformat}"
+                })
+
+    if empty:
+        release = "EMPTY"
+
+    iocage = ioc.IOCage(skip_jails=True)
+
+    try:
+        iocage.create(release, props, count, pkglist=pkglist,
+                      template=template, short=short, _uuid=_uuid,
+                      basejail=basejail, thickjail=thickjail, empty=empty)
+    except RuntimeError as err:
+        if template and "Dataset" in str(err):
+            # We want to list the available templates first
+            ioc_common.logit({
+                "level": "ERROR",
+                "message": f"Template: {release} not found!"
+            })
+            templates = ioc.IOCage().list("template")
+
+            for temp in templates:
+                ioc_common.logit({
+                    "level": "EXCEPTION",
+                    "message": f"Created Templates:\n  {temp[1]}"
+                })
+            exit(1)
+        else:
+            # Standard errors
+            ioc_common.logit({
+                "level": "EXCEPTION",
+                "message": err
+            })

commit 4c5d5ef8229b0c4835cbe3bc6f9917f3c7d03bb4

Fix travis and all flake8 errors
Travis was previously broken by c15be3d74c2e9618fa5708d7a2924a096fa11402, this restores the correct behavior and fixes all issues that came since.

FreeNAS Ticket: #64926

.travis/flake8.sh
@@ -2,7 +2,7 @@
 # Run pep8 on all .py files in all subfolders
 
 tmpafter=$(mktemp)
-find ./iocage/cli ./iocage/lib -name \*.py -exec flake8 --ignore=E127,E203,W503,F811 {} + > ${tmpafter}
+find ./iocage_cli ./iocage_lib -name \*.py -exec flake8 --ignore=E127,E203,W503,F811 {} + > ${tmpafter}
 num_errors_after=`cat ${tmpafter} | wc -l`
 echo "Current Error Count: ${num_errors_after}"
 
@@ -15,6 +15,7 @@ echo "Comparing with last stable release: ${last_release}"
 git checkout ${last_release}
 
 tmpbefore=$(mktemp)
+# TODO: Set this to the above after release of 1.0
 find ./iocage/cli ./iocage/lib -name \*.py -exec flake8 --ignore=E127,E203,W503,F811 {} + > ${tmpbefore}
 num_errors_before=`cat ${tmpbefore} | wc -l`
 echo "${last_release}'s Error Count: ${num_errors_before}"

iocage_cli/create.py
@@ -103,7 +103,7 @@ def cli(release, template, count, props, pkglist, basejail, thickjail, empty,
 
     if name:
         # noinspection Annotator
-        valid = True if re.match("^[a-zA-Z0-9\._-]+$", name) else False
+        valid = True if re.match(r"^[a-zA-Z0-9\._-]+$", name) else False
 
         if not valid:
             ioc_common.logit({

iocage_cli/get.py
@@ -35,7 +35,7 @@ import iocage_lib.iocage as ioc
 @click.argument("jail", required=True, default="")
 @click.option("--header", "-h", "-H", is_flag=True, default=True,
               help="For scripting, use tabs for separators.")
-@click.option("--recursive", "-r", help="Get the specified property for all " +
+@click.option("--recursive", "-r", help="Get the specified property for all "
                                         "jails.", flag_value="recursive")
 @click.option("--plugin", "-P",
               help="Get the specified key for a plugin jail, if accessing a"

iocage_lib/ioc_create.py
@@ -759,9 +759,9 @@ class IOCCreate(object):
                 cmd, jail_uuid, location, plugin=self.plugin, su_env=pkg_env
             ) as _exec:
                 iocage_lib.ioc_common.consume_and_log(
-                     _exec,
-                     callback=self.callback,
-                     log=not(self.silent)
+                    _exec,
+                    callback=self.callback,
+                    log=not(self.silent)
                 )
         except iocage_lib.ioc_exceptions.CommandFailed as e:
             iocage_lib.ioc_stop.IOCStop(jail_uuid, location, force=True,

iocage_lib/ioc_fetch.py
@@ -143,9 +143,8 @@ class IOCFetch(iocage_lib.ioc_json.IOCZFS):
             # Quick list validation
             try:
                 releases.index(self.release)
-            except ValueError as err:
+            except ValueError:
                 # Time to print the list again
-
                 for r in releases:
                     iocage_lib.ioc_common.logit(
                         {
@@ -936,7 +935,7 @@ class IOCFetch(iocage_lib.ioc_json.IOCZFS):
             'PWD': '/',
             'HOME': '/',
             'TERM': 'xterm-256color'
-            }
+        }
 
         if os.path.isfile(f"{mount_root}/etc/freebsd-update.conf"):
             if self.verify:
@@ -970,9 +969,9 @@ class IOCFetch(iocage_lib.ioc_json.IOCZFS):
                     _exec, callback=self.callback)
 
             fetch_install_cmd = [
-                    fetch_name, "-b", mount_root, "-d",
-                    f"{mount_root}/var/db/freebsd-update/", "-f",
-                    f"{mount_root}/etc/freebsd-update.conf", "install"
+                fetch_name, "-b", mount_root, "-d",
+                f"{mount_root}/var/db/freebsd-update/", "-f",
+                f"{mount_root}/etc/freebsd-update.conf", "install"
             ]
             with iocage_lib.ioc_exec.IOCExec(
                 fetch_install_cmd,

iocage_lib/ioc_json.py
@@ -452,8 +452,8 @@ class IOCConfiguration(IOCZFS):
                 else:
                     temp_uuid = self.location.rsplit('/', 1)[-1]
                     freebsd_version = pathlib.Path(
-                        f'{self.iocroot}/jails/{temp_uuid}' \
-                        '/root/bin/freebsd-version'
+                        f'{self.iocroot}/jails/{temp_uuid}/root/bin/'
+                        'freebsd-version'
                     )
                 if not freebsd_version.is_file():
                     iocage_lib.ioc_common.logit(
@@ -870,7 +870,7 @@ class IOCJson(IOCConfiguration):
                 self.zfs_set_property(f"{dataset}/data", jail_zfs_prop,
                                       f"iocage/jails/{uuid}/data")
                 self.zfs_set_property(f"{dataset}/data", "jailed", "on")
-            except libzfs.ZFSException as err:
+            except libzfs.ZFSException:
                 # The jailed dataset doesn't exist, which is OK.
                 pass
 
@@ -1330,8 +1330,9 @@ class IOCJson(IOCConfiguration):
                     key = key.replace("_", ".")
 
                 if key in jail_params:
-                    if full_conf["vnet"] == "on" and (key == "ip4.addr" or
-                                                      key == "ip6.addr"):
+                    if full_conf["vnet"] == "on" and (
+                        key == "ip4.addr" or key == "ip6.addr"
+                    ):
                         return
 
                     try:
@@ -1564,10 +1565,12 @@ class IOCJson(IOCConfiguration):
                                     rx,
                                     v.lower()
                                 ) for v in value.split()
-                            ) or
-                            len(value.split()) != 2 or
-                            any(value.split().count(v) > 1 for v in
-                                value.split())
+                            ) or len(
+                                value.split()
+                            ) != 2 or any(
+                                value.split().count(v) > 1 for v in
+                                value.split()
+                            )
                         ):
                             iocage_lib.ioc_common.logit(
                                 {

iocage_lib/ioc_list.py
@@ -304,7 +304,7 @@ class IOCList(object):
                                     iocage_lib.ioc_json.IOCJson(
                                         mountpoint).json_plugin_get_value(
                                         prop.split("."))
-                                    )
+                                )
                         except KeyError:
                             pass
                         except iocage_lib.ioc_exceptions.CommandFailed as e:

iocage_lib/ioc_plugin.py
@@ -246,8 +246,8 @@ class IOCPlugin(object):
                     {
                         "level": "WARNING",
                         "message":
-                         "  This plugin requires accepting a license "
-                        "to proceed:"
+                            "  This plugin requires accepting a license "
+                            "to proceed:"
                     },
                     _callback=self.callback,
                     silent=self.silent)
@@ -340,15 +340,15 @@ class IOCPlugin(object):
     def __fetch_plugin_create__(self, create_props, uuid):
         """Creates the plugin with the provided properties"""
         uuid = iocage_lib.ioc_create.IOCCreate(
-                self.release,
-                create_props,
-                0,
-                silent=True,
-                basejail=True,
-                uuid=uuid,
-                plugin=True,
-                thickconfig=self.thickconfig,
-                callback=self.callback
+            self.release,
+            create_props,
+            0,
+            silent=True,
+            basejail=True,
+            uuid=uuid,
+            plugin=True,
+            thickconfig=self.thickconfig,
+            callback=self.callback
         ).create_jail()
 
         jaildir = f"{self.iocroot}/jails/{uuid}"
@@ -622,7 +622,7 @@ fingerprint: {fingerprint}
                                     iocage_lib.ioc_json.IOCJson(
                                         jaildir).json_plugin_get_value(
                                         prop.split("."))
-                                    )
+                                )
                         except KeyError:
                             pass
 
@@ -977,12 +977,12 @@ fingerprint: {fingerprint}
                 silent=self.silent)
 
             err = iocage_lib.ioc_create.IOCCreate(
-                    self.release,
-                    "",
-                    0,
-                    pkglist=["ca_root_nss"],
-                    silent=True, callback=self.callback
-                ).create_install_packages(self.plugin, path)
+                self.release,
+                "",
+                0,
+                pkglist=["ca_root_nss"],
+                silent=True, callback=self.callback
+            ).create_install_packages(self.plugin, path)
 
             if err:
                 self.__rollback_jail__(name="update")

iocage_lib/ioc_start.py
@@ -180,8 +180,11 @@ class IOCStart(object):
             self.__check_dhcp__()
 
         if mount_procfs == "1":
-            su.Popen(["mount", "-t", "procfs", "proc", self.path +
-                      "/root/proc"]).communicate()
+            su.Popen(
+                [
+                    'mount', '-t', 'procfs', 'proc', f'{self.path}/root/proc'
+                ]
+            ).communicate()
 
         try:
             mount_linprocfs = self.conf["mount_linprocfs"]
@@ -190,8 +193,11 @@ class IOCStart(object):
                 if not os.path.isdir(f"{self.path}/root/compat/linux/proc"):
                     os.makedirs(f"{self.path}/root/compat/linux/proc", 0o755)
                 su.Popen(
-                    ["mount", "-t", "linprocfs", "linproc", self.path +
-                     "/root/compat/linux/proc"]).communicate()
+                    [
+                        'mount', '-t', 'linprocfs', 'linproc',
+                        f'{self.path}/root/compat/linux/proc'
+                    ]
+                ).communicate()
         except Exception:
             pass
 
@@ -352,31 +358,30 @@ class IOCStart(object):
             f"allow.mount.zfs={allow_mount_zfs}"
         ]
 
-        start_cmd = [x for x in ["jail", "-c"] + net +
-                     [x for x in parameters if '1' in x] +
-                     [
-                         f'name=ioc-{self.uuid}',
-                         f'host.domainname={host_domainname}',
-                         f'host.hostname={host_hostname}',
-                         f'path={self.path}/root',
-                         f'securelevel={securelevel}',
-                         f'host.hostuuid={self.uuid}',
-                         f'devfs_ruleset={devfs_ruleset}',
-                         f'enforce_statfs={enforce_statfs}',
-                         f'children.max={children_max}',
-                         f'exec.prestart={exec_prestart}',
-                         f'exec.poststart={exec_poststart}',
-                         f'exec.prestop={exec_prestop}',
-                         f'exec.stop={exec_stop}',
-                         f'exec.clean={exec_clean}',
-                         f'exec.timeout={exec_timeout}',
-                         f'stop.timeout={stop_timeout}',
-                         f'mount.fstab={self.path}/fstab',
-                         'allow.dying',
-                         f'exec.consolelog={self.iocroot}/log/ioc-'
-                         f'{self.uuid}-console.log',
-                         'persist'
-                     ] if x != '']
+        start_cmd = [x for x in ["jail", "-c"] + net + [
+            x for x in parameters if '1' in x] + [
+                f'name=ioc-{self.uuid}',
+                f'host.domainname={host_domainname}',
+                f'host.hostname={host_hostname}',
+                f'path={self.path}/root',
+                f'securelevel={securelevel}',
+                f'host.hostuuid={self.uuid}',
+                f'devfs_ruleset={devfs_ruleset}',
+                f'enforce_statfs={enforce_statfs}',
+                f'children.max={children_max}',
+                f'exec.prestart={exec_prestart}',
+                f'exec.poststart={exec_poststart}',
+                f'exec.prestop={exec_prestop}',
+                f'exec.stop={exec_stop}',
+                f'exec.clean={exec_clean}',
+                f'exec.timeout={exec_timeout}',
+                f'stop.timeout={stop_timeout}',
+                f'mount.fstab={self.path}/fstab',
+                'allow.dying',
+                f'exec.consolelog={self.iocroot}/log/ioc-'
+                f'{self.uuid}-console.log',
+                'persist'
+        ] if x != '']
 
         start_env = {
             **os.environ,
@@ -502,8 +507,8 @@ class IOCStart(object):
                     _callback=self.callback,
                     silent=self.silent)
 
-            iocage_lib.ioc_stop.IOCStop(self.uuid, self.path, force=True,
-                                        silent=True
+            iocage_lib.ioc_stop.IOCStop(
+                self.uuid, self.path, force=True, silent=True
             )
 
             iocage_lib.ioc_common.logit({
@@ -605,9 +610,9 @@ class IOCStart(object):
 
         vnet_default_interface = self.get('vnet_default_interface')
         if (
-                vnet_default_interface != 'auto' and
-                vnet_default_interface != 'none' and
-                vnet_default_interface not in netifaces.interfaces()
+                vnet_default_interface != 'auto'
+                and vnet_default_interface != 'none'
+                and vnet_default_interface not in netifaces.interfaces()
         ):
             # Let's not go into starting a vnet at all if the default
             # interface is supplied incorrectly
@@ -916,7 +921,7 @@ class IOCStart(object):
         else:
             try:
                 mac_a, mac_b = mac.replace(',', ' ').split()
-            except Exception as e:
+            except Exception:
                 iocage_lib.ioc_common.logit({
                     "level": "EXCEPTION",
                     "message": f'Please correct mac addresses format for {nic}'

iocage_lib/ioc_stop.py
@@ -188,8 +188,8 @@ class IOCStop(object):
                                     err.output.decode("utf-8").rstrip()))
 
         # We should still try to destroy the relevant networking
-        # related resources if force is true, though we won't raise an exception
-        # in that case
+        # related resources if force is true, though we won't raise an
+        # exception in that case
         # They haven't set an IP address, this interface won't exist
         destroy_nic = True if dhcp == "on" or ip4_addr != "none" or \
             ip6_addr != "none" else False
@@ -249,9 +249,9 @@ class IOCStop(object):
                         iface, addr = ip4.split("/")[0].split("|")
                         addr = addr.split()
                         iocage_lib.ioc_common.checkoutput(
-                            ["ifconfig", iface] + addr +
-                            ["-alias"],
-                            stderr=su.STDOUT)
+                            ['ifconfig', iface] + addr + ['-alias'],
+                            stderr=su.STDOUT
+                        )
                     except su.CalledProcessError as err:
                         if "Can't assign requested address" in \
                                 err.output.decode("utf-8"):
@@ -275,8 +275,9 @@ class IOCStop(object):
                         iface, addr = ip6.split("/")[0].split("|")
                         addr = addr.split()
                         iocage_lib.ioc_common.checkoutput(
-                            ["ifconfig", iface, "inet6"] + addr +
-                            ["-alias"], stderr=su.STDOUT)
+                            ["ifconfig", iface, "inet6"] + addr + ["-alias"],
+                            stderr=su.STDOUT
+                        )
                     except su.CalledProcessError as err:
                         if "Can't assign requested address" in \
                                 err.output.decode("utf-8"):
@@ -304,8 +305,9 @@ class IOCStop(object):
         # 4 is a placeholder for iocage jails, if the ruleset doesn't exist in
         # ruleset_list, it was generated by iocage as the user supplied a
         # non-existent devfs rule. We still want to clean that up.
-        if ruleset != '4' and (devfs_ruleset == '4' or
-                               int(devfs_ruleset) not in ruleset_list):
+        if ruleset != '4' and (
+            devfs_ruleset == '4' or int(devfs_ruleset) not in ruleset_list
+        ):
             try:
                 su.run(
                     ['devfs', 'rule', '-s', ruleset, 'delset'],
@@ -319,7 +321,7 @@ class IOCStop(object):
                 },
                     _callback=self.callback,
                     silent=self.silent)
-            except su.CalledProcessError as err:
+            except su.CalledProcessError:
                 msg = f'  + Removing devfs_ruleset: {ruleset} FAILED'
                 iocage_lib.ioc_common.logit({
                     "level": 'ERROR',

iocage_lib/ioc_upgrade.py
@@ -74,7 +74,7 @@ class IOCUpgrade(object):
             'PWD': '/',
             'HOME': '/',
             'TERM': 'xterm-256color'
-            }
+        }
 
         self.callback = callback
 
@@ -113,11 +113,11 @@ class IOCUpgrade(object):
             os.chmod(tmp.name, 0o755)
 
             fetch_cmd = [
-                    tmp.name, "-b", self.path, "-d",
-                    f"{self.path}/var/db/freebsd-update/", "-f",
-                    f"{self.path}/etc/freebsd-update.conf",
-                    "--not-running-from-cron", "--currently-running "
-                    f"{self.jail_release}", "-r", self.new_release, "upgrade"
+                tmp.name, "-b", self.path, "-d",
+                f"{self.path}/var/db/freebsd-update/", "-f",
+                f"{self.path}/etc/freebsd-update.conf",
+                "--not-running-from-cron", "--currently-running "
+                f"{self.jail_release}", "-r", self.new_release, "upgrade"
             ]
             with iocage_lib.ioc_exec.IOCExec(
                 fetch_cmd,
@@ -293,10 +293,10 @@ class IOCUpgrade(object):
     def __upgrade_install__(self, name):
         """Installs the upgrade and returns the exit code."""
         install_cmd = [
-                name, "-b", self.path, "-d",
-                f"{self.path}/var/db/freebsd-update/", "-f",
-                f"{self.path}/etc/freebsd-update.conf", "-r", self.new_release,
-                "install"
+            name, "-b", self.path, "-d",
+            f"{self.path}/var/db/freebsd-update/", "-f",
+            f"{self.path}/etc/freebsd-update.conf", "-r", self.new_release,
+            "install"
         ]
 
         with iocage_lib.ioc_exec.IOCExec(

iocage_lib/iocage.py
@@ -782,8 +782,8 @@ class IOCage(object):
         exec_clean = self.get('exec_clean')
 
         if exec_clean == '1':
-            env_path = '/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:'\
-                   '/usr/local/bin:/root/bin'
+            env_path = '/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:' \
+                '/usr/local/bin:/root/bin'
             env_lang = os.environ.get('LANG', 'en_US.UTF-8')
             su_env = {
                 'PATH': env_path,
@@ -792,7 +792,7 @@ class IOCage(object):
                 'TERM': 'xterm-256color',
                 'LANG': env_lang,
                 'LC_ALL': env_lang
-                }
+            }
         else:
             su_env = os.environ.copy()
 
@@ -843,7 +843,7 @@ class IOCage(object):
             interactive_cmd = (
                 "/usr/sbin/setfib", exec_fib,
                 "jexec", f"ioc-{uuid.replace('.', '_')}"
-                ) + command
+            ) + command
 
             try:
                 _started = False
@@ -868,8 +868,8 @@ class IOCage(object):
                     raise ioc_exceptions.CommandFailed(
                         f'Command: {command}, stdout: {stdout},'
                         f' stderr: {stderr}'
-                        )
-            except Exception as e:
+                    )
+            except Exception:
                 raise
             return
 
@@ -995,8 +995,9 @@ class IOCage(object):
                     branch=branch,
                     thickconfig=thick_config
                 ).fetch_plugin_index(
-                        "", _list=True, list_header=header, list_long=_long,
-                    icon=True, official=official)
+                    "", _list=True, list_header=header, list_long=_long,
+                    icon=True, official=official
+                )
 
                 return rel_list
 
@@ -2004,11 +2005,13 @@ Remove the snapshot: ioc_upgrade_{_date} if everything is OK
             msg = f"\n{uuid} successfully upgraded from" \
                 f" {jail_release} to {new_release}!"
 
-        ioc_common.logit({
-            "level": "INFO",
-            "message": msg
-        },
-                _callback=self.callback)
+        ioc_common.logit(
+            {
+                'level': 'INFO',
+                'message': msg
+            },
+            _callback=self.callback
+        )
 
     def debug(self, directory):
         if directory is None: