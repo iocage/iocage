3180b3e6270f082edc12169a209fd77afb8ee4a5
Waqar Ahmed <waqarsonic1@gmail.com>
2019-01-14 03:32:19 +0500

    Improve exception handling
    This commit improves how we handle exception handling and also fixes an issue where we could not return values from existing exception handler.

--- iocage_lib/ioc_exceptions.py
+++ iocage_lib/ioc_exceptions.py
@@ -76,16 +76,18 @@ class ValueNotFound(Exception):
 
 
 @contextmanager
-def ignore_exceptions(*exceptions, return_value=None, suppress_exception=True):
+def ignore_exceptions(*exceptions, clean=None, suppress_exception=True):
+    """
+    Ignore any exceptions specified by `exceptions` and make sure that
+    we clean any resources specified by callable `clean`
+    """
     try:
         yield
     except exceptions as e:
+        if callable(clean):
+            return clean()
+
         if not suppress_exception:
             # For cases where this block is used dynamically to suppress
             # exceptions
             raise e
-
-        if callable(return_value):
-            return return_value()
-        else:
-            return return_value
