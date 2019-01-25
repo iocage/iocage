3e0c61ada9a22e273590e5c579e65928c61c59b3
Brandon Schneider <brandon@ixsystems.com>
2019-01-25 15:32:55 -0600

    Bump to 1.1-RELEASE

--- iocage_cli/__init__.py
+++ iocage_cli/__init__.py
@@ -63,7 +63,7 @@ def print_version(ctx, param, value):
 
     if not value or ctx.resilient_parsing:
         return
-    print("Version\t1.1 PRERELEASE 2019/01")
+    print("Version\t1.1 RELEASE 2019/01")
     sys.exit()
 
 
