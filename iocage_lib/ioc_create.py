8e052dcec5956c0a78d5379fd38bd37982b31560
Brandon Schneider <brandon@ixsystems.com>
2019-02-05 15:53:25 -0600

    tkt-73740: Add ip_hostname support to iocage (#847)
    * Add ip_hostname support to iocage
    
    If a user sets their DNS records up and they match the jail name, this bool will allow the user to skip configuring the IP manually, as jail(8) will grab the IP from the resolver.
    
    FreeNAS Ticket: #73740
    
    * Allow fetching plugins and pkglist with this
    
    - Users may still be able to footshoot as iocage does not verify they indeed have correctly set an IP address in their DNS.
    - Also fix a bad error message
    
    * Address Waqar's review

--- iocage_lib/ioc_create.py
+++ iocage_lib/ioc_create.py
@@ -611,11 +611,11 @@ class IOCCreate(object):
                 silent=self.silent)
 
         if self.pkglist:
-            if config.get('ip4_addr', 'none') == 'none' and \
-                config.get('ip6_addr', 'none') == 'none' and \
-                    not iocage_lib.ioc_common.check_truthy(
-                        config.get('dhcp', 0)
-            ):
+            dhcp_or_hostname = config.get('dhcp') or config.get('ip_hostname')
+
+            if config.get('ip4_addr', 'none') == "none" and \
+                config.get('ip6_addr', 'none') == "none" and \
+                    not dhcp_or_hostname:
                 iocage_lib.ioc_common.logit({
                     "level": "WARNING",
                     "message": "You need an IP address for the jail to"
--- iocage_lib/ioc_plugin.py
+++ iocage_lib/ioc_plugin.py
@@ -361,11 +361,12 @@ class IOCPlugin(object):
         path = f"{self.pool}/iocage/jails/{uuid}"
         _conf = iocage_lib.ioc_json.IOCJson(jaildir).json_get_value('all')
 
-        # We do this test again as the user could supply a malformed IP to
+        # We do these tests again as the user could supply a malformed IP to
         # fetch that bypasses the more naive check in cli/fetch
+        dhcp_or_hostname = _conf['dhcp'] or _conf['ip_hostname']
 
         if _conf["ip4_addr"] == "none" and _conf["ip6_addr"] == "none" and \
-           not iocage_lib.ioc_common.check_truthy(_conf['dhcp']):
+           not dhcp_or_hostname:
             iocage_lib.ioc_common.logit(
                 {
                     "level": "ERROR",
