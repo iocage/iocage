12dcde94bd003b51d6e7d53e4060f8d626dd0546
Waqar Ahmed <waqarsonic1@gmail.com>
2019-02-08 18:27:53 +0500

    Add Cpuset support to Iocage
    This commit introduces the following changes:
    1) A class to manage Cpuset prop wrt
      a) Validation
      b) Setting Cpuset Prop
    2) Ability to set cpuset prop conforming to cpuset(1) format for '-l' flag only.
    3) We only allow a very basic version of cpuset(1) support in which a user can specify a list of CPUs to apply to the jail.
    4) When a jail is started, we set the cpuset prop in the system only at that point allowing the user to change the prop when the jail is running but not reflecting that change in the system. That change is only reflected in the system when jail is restarted. Motivation behind this step is that we are more likely to fail setting the prop when the jail is running in the system because of possible deadlock/validation issues raised by cpuset(1).
    5) Point 3 and 4 can be revisited later to allow more support or enhance the functionality gauging user reaction/usage.
    
    Ticket: #74235

--- tests/functional_tests/0006_set_test.py
+++ tests/functional_tests/0006_set_test.py
@@ -23,6 +23,7 @@
 # POSSIBILITY OF SUCH DAMAGE.
 
 import pytest
+import re
 
 
 require_root = pytest.mark.require_root
@@ -37,13 +38,13 @@ require_zpool = pytest.mark.require_zpool
 # TODO: Plugin test left
 
 
-def _set_and_test_note_prop(invoke_cli, value, jail):
+def _set_and_test_prop(invoke_cli, value, jail, prop='notes'):
     invoke_cli(
-        ['set', f'notes={value}', jail.name]
+        ['set', f'{prop}={value}', jail.name]
     )
 
-    assert jail.config.get('notes') == value, \
-        f'Failed to set note value to {value}'
+    assert jail.config.get(prop) == value, \
+        f'Failed to set {prop} value to {value}'
 
 
 @require_root
@@ -52,7 +53,7 @@ def test_01_set_prop_on_jail(resource_selector, invoke_cli, skip_test):
     jails = resource_selector.jails
     skip_test(not jails)
 
-    _set_and_test_note_prop(
+    _set_and_test_prop(
         invoke_cli, 'foo \"bar\"', jails[0]
     )
 
@@ -65,7 +66,7 @@ def test_02_set_prop_on_thickconfig_jail(
     thickconfig_jails = resource_selector.thickconfig_jails
     skip_test(not thickconfig_jails)
 
-    _set_and_test_note_prop(
+    _set_and_test_prop(
         invoke_cli, 'foo \"bar\"', thickconfig_jails[0]
     )
 
@@ -76,7 +77,7 @@ def test_03_set_prop_on_basejail(resource_selector, invoke_cli, skip_test):
     basejails = resource_selector.basejails
     skip_test(not basejails)
 
-    _set_and_test_note_prop(
+    _set_and_test_prop(
         invoke_cli, 'foo \"bar\"', basejails[0]
     )
 
@@ -87,6 +88,74 @@ def test_04_set_prop_on_template_jail(resource_selector, invoke_cli, skip_test):
     template_jails = resource_selector.template_jails
     skip_test(not template_jails)
 
-    _set_and_test_note_prop(
+    _set_and_test_prop(
         invoke_cli, 'foo \"bar\"', template_jails[0]
     )
+
+
+@require_root
+@require_zpool
+def test_04_set_cpuset_prop_on_jail(
+    resource_selector, invoke_cli, skip_test, run_console
+):
+    jails = resource_selector.startable_jails_and_not_running
+    skip_test(not jails)
+
+    # We need to get the no of cpus first
+    cpuset_output = run_console(['cpuset', '-g', '-s', '0'])
+    skip_test(cpuset_output.returncode)
+
+    cpuset_num = re.findall(
+        r'.*mask:.*(\d+)$',
+        cpuset_output.stdout.decode().split('\n')[0]
+    )
+    skip_test(not cpuset_num)
+
+    cpuset_num = int(cpuset_num[0])
+
+    # We would like to test following formats now
+    # 0,1,2,3
+    # 0-2
+    # all
+    # off
+    #
+    # However if the num of cpu is only one, we can't do ranges or multiple
+    # values in that case
+
+    possible_variations = ['off', 'all']
+    if cpuset_num:
+        possible_variations.extend([
+            f'0-{cpuset_num}',
+            f','.join(
+                map(
+                    str,
+                    range(cpuset_num + 1)
+                )
+            )
+        ])
+
+    jail = jails[0]
+    for variation in possible_variations:
+        _set_and_test_prop(
+            invoke_cli, variation, jail, 'cpuset'
+        )
+
+    if cpuset_num:
+        invoke_cli(
+            ['start', jail.name],
+            f'Jail {jail} failed to start'
+        )
+
+        assert jail.running is True
+
+        jail_cpusets = jail.cpuset
+        assert set(jail_cpusets) == set(
+            map(int, possible_variations[-1].split(','))
+        )
+
+        invoke_cli(
+            ['stop', jail.name],
+            f'Jail {jail} failed to stop'
+        )
+
+        assert jail.running is False
