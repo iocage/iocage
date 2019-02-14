9f73e972ad68818f221fa5b2845a6636eb769195
Brandon Schneider <brandon@ixsystems.com>
2019-02-13 18:32:29 -0600

    Drop dulwich dependency, add docurl support to ui.json for plugins
    - Replace Dulwich with GitPython
    We were experiencing bad attribute usage with Dulwich, and while upstream has a commit to fix this, it didn't solve for our situation. Since we've had a couple problems with Dulwich in the past, let's try switching libraries to something that has been around longer.
    
    - Also added docurl support to ui.json, this allows a plugin author to additionally specify where a user might be able to view documentation from the program authors themselves.
    
    FreeNAS Ticket: #72323

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'dulwich>=0.18.6',
+        'GitPython>=2.1.11',
         'netifaces>=0.10.8',
         'dnspython>=1.15.0',
         'libzfs'
