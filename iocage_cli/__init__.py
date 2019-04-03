2cc4aa48a73140570e798bfa9f06e7abab41d66a
Brandon Schneider <brandon@ixsystems.com>
2019-04-03 12:55:37 -0500

    We're close to a RELEASE (#904)

--- iocage_cli/__init__.py
+++ iocage_cli/__init__.py
@@ -66,7 +66,7 @@ def print_version(ctx, param, value):
 
     if not value or ctx.resilient_parsing:
         return
-    print("Version\t1.2 BETA")
+    print("Version\t1.2 RC")
     sys.exit()
 
 
