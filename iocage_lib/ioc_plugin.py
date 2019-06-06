70b53cd1e68c644df7a258f36c580fa09fb5b19b
Waqar Ahmed <waqarsonic1@gmail.com>
2019-06-06 22:03:04 +0500

    Properly remove plugin index directory
    os.rmdir will fail to remove directory if it is not empty.
    Ticket: #NAS-102032

--- iocage_lib/ioc_plugin.py
+++ iocage_lib/ioc_plugin.py
@@ -1361,7 +1361,7 @@ fingerprint: {fingerprint}
         ):
             # Clone
             try:
-                os.rmdir(destination)
+                shutil.rmtree(destination)
             except FileNotFoundError:
                 pass
             finally:
