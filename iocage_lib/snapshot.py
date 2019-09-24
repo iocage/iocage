0895315937f76ec37e64f75ddf9780ed322377d1
Waqar Ahmed <waqarahmedjoyia@live.com>
2019-09-24 12:21:23 +0500

    Add ability to retrieve release snapshots

--- iocage_lib/snapshot.py
+++ iocage_lib/snapshot.py
@@ -1,5 +1,10 @@
+from iocage_lib.dataset import Dataset
 from iocage_lib.resource import Resource, ListableResource
-from iocage_lib.zfs import list_snapshots, destroy_zfs_resource
+from iocage_lib.zfs import (
+    list_snapshots, destroy_zfs_resource, iocage_activated_dataset
+)
+
+import pathlib
 
 
 class Snapshot(Resource):
@@ -26,3 +31,14 @@ class SnapshotListableResource(ListableResource):
     def __iter__(self):
         for snap in list_snapshots():
             yield self.resource(snap)
+
+    @property
+    def release_snapshots(self):
+        # Returns all jail snapshots on each RELEASE dataset
+        iocage_dataset = Dataset(iocage_activated_dataset())
+        if iocage_dataset.exists:
+            rel_dir = pathlib.Path(f'{iocage_dataset.path}/releases')
+
+            # Quicker than asking zfs and parsing
+            for snap in rel_dir.glob('**/root/.zfs/snapshot/*'):
+                yield Snapshot(snap.name)
