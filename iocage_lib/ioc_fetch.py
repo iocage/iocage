c37ff2b6ae596b3a4bfe94d758ed2a49e7401811
Waqar Ahmed <waqarahmedjoyia@live.com>
2019-09-25 17:00:11 +0500

    Only check tmp dataset exec property if it exists, this was implicit before

--- iocage_lib/ioc_fetch.py
+++ iocage_lib/ioc_fetch.py
@@ -819,16 +819,17 @@ class IOCFetch:
     def fetch_update(self, cli=False, uuid=None):
         """This calls 'freebsd-update' to update the fetched RELEASE."""
         tmp_dataset = Dataset('/tmp')
-        tmp_val = tmp_dataset.properties['exec']
+        if tmp_dataset.exists:
+            tmp_val = tmp_dataset.properties['exec']
 
-        if tmp_val == 'off':
-            iocage_lib.ioc_common.logit(
-                {
-                    'level': 'EXCEPTION',
-                    'message': f'{tmp_dataset.name} needs exec=on!'
-                },
-                _callback=self.callback,
-                silent=self.silent)
+            if tmp_val == 'off':
+                iocage_lib.ioc_common.logit(
+                    {
+                        'level': 'EXCEPTION',
+                        'message': f'{tmp_dataset.name} needs exec=on!'
+                    },
+                    _callback=self.callback,
+                    silent=self.silent)
 
         if cli:
             cmd = [
