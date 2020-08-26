eccb525ff891655323cc41ba91c273f08422cbde
Waqar Ahmed <waqarahmedjoyia@live.com>
2020-08-27 01:09:20 +0500

    Add method to retrieve all dependents in cache

--- iocage_lib/cache.py
+++ iocage_lib/cache.py
@@ -1,7 +1,7 @@
 import os
 
 from iocage_lib.zfs import (
-    all_properties, dataset_exists, iocage_activated_pool
+    all_properties, dataset_exists, iocage_activated_pool, get_all_dependents,
 )
 
 import threading
@@ -12,7 +12,7 @@ class Cache:
     cache_lock = threading.Lock()
 
     def __init__(self):
-        self.dataset_data = self.pool_data = None
+        self.dataset_data = self.pool_data = self.dataset_dep_data = None
 
     @property
     def datasets(self):
@@ -29,6 +29,18 @@ class Cache:
                     self.dataset_data.update(all_properties(ioc_pool, types=['filesystem']))
             return self.dataset_data
 
+    def dependents(self, dataset):
+        with self.cache_lock:
+            if not self.dataset_dep_data:
+                self.dataset_dep_data = {}
+                for ds in get_all_dependents():
+                    self.dataset_dep_data[ds] = []
+                    for k in self.dataset_dep_data:
+                        if ds.startswith(k):
+                            self.dataset_dep_data[k].append(ds)
+
+            return self.dataset_dep_data.get(dataset, [])
+
     def update_dataset_data(self, dataset, props):
         with self.cache_lock:
             self.dataset_data[dataset] = props
@@ -42,7 +54,7 @@ class Cache:
 
     def reset(self):
         with self.cache_lock:
-            self.dataset_data = self.pool_data = None
+            self.dataset_data = self.pool_data = self.dataset_dep_data = None
 
 
 cache = Cache()
