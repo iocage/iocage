a56694afdbf084c2603dc6b775daa686e8cd499f
fulder <misad90@gmail.com>
2021-04-18 00:21:58 +0200

    Add unittest for validate_plugin_manifest func

---
+++ tests/unit_tests/1004_lib_common.py
@@ -0,0 +1,23 @@
+from iocage_lib.ioc_common import validate_plugin_manifest
+
+VALID_MANIFEST = {
+    "name": "test_plugin",
+    "release": "12.2-RELEASE",
+    "pkgs": [],
+    "packagesite": "http://pkg.FreeBSD.org/${ABI}/latest",
+    "fingerprints": {
+        "iocage-plugins": [
+            {
+                "function": "sha256",
+                "fingerprint": "b0170035af3acc5f3f3ae1859dc717101b4e6c1d0a794ad554928ca0cbb2f438"
+            }
+        ]
+    },
+}
+
+def log_callback(content, exception):
+    print(content)
+    print(exception)
+
+def test_validate_plugin_manifest():
+    validate_plugin_manifest(VALID_MANIFEST, log_callback, None)
