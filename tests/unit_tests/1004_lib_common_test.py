a5ab463c5d449af4990d7cd32de19a4c044b8682
fulder <misad90@gmail.com>
2021-04-18 00:41:03 +0200

    Add test for missing required fields

--- tests/unit_tests/1004_lib_common_test.py
+++ tests/unit_tests/1004_lib_common_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from iocage_lib.ioc_common import validate_plugin_manifest
 
 VALID_MANIFEST = {
@@ -19,3 +21,15 @@ VALID_MANIFEST = {
 
 def test_validate_plugin_manifest():
     validate_plugin_manifest(VALID_MANIFEST, None, None)
+
+@pytest.mark.parametrize(
+    "missing_field",
+    ["name", "release", "pkgs", "packagesite", "fingerprints", "artifact"]
+)
+def test_missing_required_fields(missing_field):
+    manifest = VALID_MANIFEST.copy()
+    del manifest[missing_field]
+
+    exp_msg = "Missing \"{missing_field}\" key in manifest"
+    with pytest.raises(RuntimeError, match=exp_msg):
+        validate_plugin_manifest(VALID_MANIFEST, None, None)
