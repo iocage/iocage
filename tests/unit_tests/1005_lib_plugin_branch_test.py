f819b7d36791e53f5cdc18ecb0869315fc6f4833
fulder <misad90@gmail.com>
2021-04-25 00:50:51 +0200

    Add branch test for hardened plugin

--- tests/unit_tests/1005_lib_plugin_branch_test.py
+++ tests/unit_tests/1005_lib_plugin_branch_test.py
@@ -4,15 +4,6 @@ from iocage_lib.ioc_plugin import IOCPlugin
 
 
 @patch("iocage_lib.ioc_json.IOCJson")
-def test_set_plugin_branch(mocked_ioc_json):
-    test_branch = "TEST_BRANCH"
-    plugin = IOCPlugin(branch=test_branch)
-
-    assert plugin.index_branch == test_branch
-    assert plugin.__get_plugin_branch__({}) == test_branch
-
-
-@patch("iocage_lib.ioc_json.IOCJson")
 @patch("iocage_lib.cache.Cache.freebsd_version", new_callable=PropertyMock)
 def test_default_branch_values(mocked_cache, mocked_ioc_json):
     mocked_cache.return_value = "12.2"
@@ -24,8 +15,17 @@ def test_default_branch_values(mocked_cache, mocked_ioc_json):
 
 
 @patch("iocage_lib.ioc_json.IOCJson")
+def test_set_plugin_branch(mocked_ioc_json):
+    test_branch = "TEST_BRANCH"
+    plugin = IOCPlugin(branch=test_branch)
+
+    assert plugin.index_branch == test_branch
+    assert plugin.__get_plugin_branch__({}) == test_branch
+
+
+@patch("iocage_lib.ioc_json.IOCJson")
 @patch("iocage_lib.cache.Cache.freebsd_version", new_callable=PropertyMock)
-def test_default_branch_values(mocked_cache, mocked_ioc_json):
+def test_plugin_manifest_branch(mocked_cache, mocked_ioc_json):
     mocked_cache.return_value = "12.2"
     plugin_branch = "plugin_branch"
     plugin_manifest = {"branch": plugin_branch}
@@ -34,3 +34,14 @@ def test_default_branch_values(mocked_cache, mocked_ioc_json):
 
     assert plugin.index_branch == "12.2-RELEASE"
     assert plugin.__get_plugin_branch__(plugin_manifest) == plugin_branch
+
+
+@patch("iocage_lib.ioc_json.IOCJson")
+@patch("iocage_lib.cache.Cache.freebsd_version", new_callable=PropertyMock)
+def test_hardened_plugin(mocked_cache, mocked_ioc_json):
+    mocked_cache.return_value = "12.2"
+
+    plugin = IOCPlugin(hardened=True)
+
+    assert plugin.index_branch == "master"
+    assert plugin.__get_plugin_branch__({}) == "master"
