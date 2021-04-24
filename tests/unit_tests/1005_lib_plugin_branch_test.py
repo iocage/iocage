632035e257d401158bc632f3dfcac6e879355a1b
fulder <misad90@gmail.com>
2021-04-25 00:47:40 +0200

    Add test for getting plugin branch from manifest

--- tests/unit_tests/1005_lib_plugin_branch_test.py
+++ tests/unit_tests/1005_lib_plugin_branch_test.py
@@ -2,11 +2,10 @@ from unittest.mock import patch, PropertyMock
 
 from iocage_lib.ioc_plugin import IOCPlugin
 
-test_branch = "TEST_BRANCH"
-
 
 @patch("iocage_lib.ioc_json.IOCJson")
 def test_set_plugin_branch(mocked_ioc_json):
+    test_branch = "TEST_BRANCH"
     plugin = IOCPlugin(branch=test_branch)
 
     assert plugin.index_branch == test_branch
@@ -24,4 +23,14 @@ def test_default_branch_values(mocked_cache, mocked_ioc_json):
     assert plugin.__get_plugin_branch__({}) == "master"
 
 
+@patch("iocage_lib.ioc_json.IOCJson")
+@patch("iocage_lib.cache.Cache.freebsd_version", new_callable=PropertyMock)
+def test_default_branch_values(mocked_cache, mocked_ioc_json):
+    mocked_cache.return_value = "12.2"
+    plugin_branch = "plugin_branch"
+    plugin_manifest = {"branch": plugin_branch}
 
+    plugin = IOCPlugin()
+
+    assert plugin.index_branch == "12.2-RELEASE"
+    assert plugin.__get_plugin_branch__(plugin_manifest) == plugin_branch
