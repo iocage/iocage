9326a2ef598e45fd5d128c4f7caaf55b33b32810
fulder <misad90@gmail.com>
2021-04-24 23:59:29 +0200

    Add test for default branch values

--- tests/unit_tests/1005_lib_plugin_branch_test.py
+++ tests/unit_tests/1005_lib_plugin_branch_test.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest.mock import patch, PropertyMock
 
 from iocage_lib.ioc_plugin import IOCPlugin
 
@@ -11,3 +11,17 @@ def test_set_plugin_branch(mocked_ioc_json):
 
     assert plugin.index_branch == test_branch
     assert plugin.__get_plugin_branch__({}) == test_branch
+
+
+@patch("iocage_lib.ioc_json.IOCJson")
+@patch("iocage_lib.cache.Cache.freebsd_version", new_callable=PropertyMock)
+def test_default_branch_values(mocked_cache, mocked_ioc_json):
+    mocked_cache.return_value = "12.2"
+
+    plugin = IOCPlugin()
+
+    assert plugin.index_branch == "12.2-RELEASE"
+    assert plugin.__get_plugin_branch__({}) == "master"
+
+
+
